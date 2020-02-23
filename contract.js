20e15bede55fb3c6e6605c2e4b99e3801e0344e6
Avaer Kazmer <a@modules.io>
2020-02-23 10:17:01 -0500

    Add missing contract source files

---
+++ contract.js
@@ -0,0 +1,35 @@
+import {makePromise} from './util.js';
+import address from './address.js';
+import abi from './abi.js';
+
+const contract = {
+  instance: null,
+  account: null,
+  promise: makePromise(),
+  async init() {
+    if (window.ethereum) {
+      window.web3 = new window.Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+        // Acccounts now exposed
+        // web3.eth.sendTransaction({/* ... */});
+
+        this.instance = window.web3.eth.contract(abi).at(address);
+        this.account = window.web3.eth.accounts[0];
+
+        this.promise.accept(this.instance);
+      } catch (err) {
+        // User denied account access...
+        console.warn(err);
+      }
+    } else {
+      console.warn('no ethereum!');
+    }
+  },
+  async getInstance() {
+    return await this.promise;
+  },
+};
+window.contract = contract;
+export default contract;
---
+++ util.js
@@ -0,0 +1,10 @@
+export function makePromise() {
+  let accept, reject;
+  const p = new Promise((a, r) => {
+    accept = a;
+    reject = r;
+  });
+  p.accept = accept;
+  p.reject = reject;
+  return p;
+};

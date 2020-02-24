d59fc93432e35c6937f3444397920e067f35089f
Avaer Kazmer <a@modules.io>
2020-02-24 03:11:40 -0500

    Change index.js address

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.static(__dirname));
 
 http.createServer(app)
-  .listen(8000);
+  .listen(3000);
 
-console.log(`http://127.0.0.1:8000`);
+console.log(`http://127.0.0.1:3000`);

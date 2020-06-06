d183da6105512def63ebf58e8698cf0405a98516
Avaer Kazmer <a@modules.io>
2020-06-06 17:57:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(express.static(__dirname));
 
 http.createServer(app)

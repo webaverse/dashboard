824dc9702527027a22a54aef4908c0f6a14ef40f
Avaer Kazmer <a@modules.io>
2020-06-10 15:23:24 -0400

    Add wbn source references

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ const _findObject = (o, name) => {
     renderer.setAnimationLoop(animate);
   }
 
-  /* {
+  /* { https://github.com/avaer/doggo
     const res = await fetch('./doggo/a.wbn');
     const ab = await res.arrayBuffer();
     const uint8Array = new Uint8Array(ab);
@@ -93,7 +93,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     pe.add(p);
   }
-  {
+  { https://github.com/avaer/terrain
     const res = await fetch('./terrain/a.wbn');
     const ab = await res.arrayBuffer();
     const uint8Array = new Uint8Array(ab);
@@ -101,7 +101,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     pe.add(p);
   }
-  {
+  { https://github.com/avaer/tree
     const res = await fetch('./tree/a.wbn');
     const ab = await res.arrayBuffer();
     const uint8Array = new Uint8Array(ab);
@@ -109,7 +109,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, -30, -31), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     pe.add(p);
   } */
-  {
+  { // https://github.com/avaer/multitree
     console.log('load multitree');
     const res = await fetch('./augs/multitree/a.wbn');
     const ab = await res.arrayBuffer();
@@ -118,7 +118,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/grass
     console.log('load grass');
     const res = await fetch('./augs/grass/a.wbn');
     const ab = await res.arrayBuffer();
@@ -127,7 +127,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, -30, -31), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/chest
     console.log('load chest');
     const res = await fetch('./augs/chest/a.wbn');
     const ab = await res.arrayBuffer();
@@ -136,7 +136,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/sprite
     console.log('load sprite');
     const res = await fetch('./augs/sprite/a.wbn');
     const ab = await res.arrayBuffer();
@@ -145,7 +145,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/miku
     console.log('load miku');
     const res = await fetch('./augs/miku/a.wbn');
     const ab = await res.arrayBuffer();
@@ -154,7 +154,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/skybox
     console.log('load skybox');
     const res = await fetch('./augs/skybox/a.wbn');
     const ab = await res.arrayBuffer();
@@ -163,7 +163,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/ocean
     console.log('load ocean');
     const res = await fetch('./augs/ocean/a.wbn');
     const ab = await res.arrayBuffer();
@@ -172,7 +172,7 @@ const _findObject = (o, name) => {
     p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
     await pe.add(p);
   }
-  {
+  { // https://github.com/avaer/cloud
     console.log('load cloud');
     const res = await fetch('./augs/cloud/a.wbn');
     const ab = await res.arrayBuffer();

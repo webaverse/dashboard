e694cecaf13baa0f03547ddea30293b6091a5bdc
Shubham Jain <shubham@sjain.dev>
2020-06-19 10:54:05 +0100

    Wrap each package loading into async function
    This simplifes the code and removes the need to use .then chaining.

--- app.js
+++ app.js
@@ -22,105 +22,87 @@ const localMatrix = new THREE.Matrix4();
 
   pe.orbitControls.target.set(0, 3, 0);
 
-  {
+  (async () => {
     console.log('load logo');
-    fetch('./augs/webaverse-logo/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/multitree
+    const res = await fetch('./augs/webaverse-logo/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/multitree
     console.log('load multitree');
-    fetch('./augs/multitree/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/grass
+    const res = await fetch('./augs/multitree/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/grass
     console.log('load grass');
-    fetch('./augs/grass/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, -30, -31), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/chest
+    const res = await fetch('./augs/grass/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, -30, -31), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/chest
     console.log('load chest');
-    fetch('./augs/chest/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/sprite
+    const res = await fetch('./augs/chest/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/sprite
     console.log('load sprite');
-    fetch('./augs/sprite/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/miku
+    const res = await fetch('./augs/sprite/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/miku
     console.log('load miku');
-    fetch('./augs/miku/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/skybox
+    const res = await fetch('./augs/miku/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/skybox
     console.log('load skybox');
-    fetch('./augs/skybox/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    })
-  }
-  { // https://github.com/avaer/ocean
+    const res = await fetch('./augs/skybox/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/ocean
     console.log('load ocean');
-    fetch('./augs/ocean/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
-  { // https://github.com/avaer/cloud
+    const res = await fetch('./augs/ocean/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
+  (async () => { // https://github.com/avaer/cloud
     console.log('load cloud');
-    fetch('./augs/cloud/a.wbn').then(res => {
-      res.arrayBuffer().then(ab => {
-        const uint8Array = new Uint8Array(ab);
-        const p = new XRPackage(uint8Array);
-        p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
-        pe.add(p);
-      });
-    });
-  }
+    const res = await fetch('./augs/cloud/a.wbn');
+    const ab = await res.arrayBuffer();
+    const uint8Array = new Uint8Array(ab);
+    const p = new XRPackage(uint8Array);
+    p.setMatrix(localMatrix.compose(localVector.set(0, 0, 0), localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1)));
+    await pe.add(p);
+  })();
 
   let currentSession = null;
   function onSessionStarted(session) {

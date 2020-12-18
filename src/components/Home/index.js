2daec527d082275400ae33b72ca3cceacba6b995
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-17 22:33:14 -0600

    add home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,20 @@ export default () =>
   <>
     <h1>Webaverse</h1>
     <p>
-      you can goto <a href="/profile">cryptoprofile</a>
+      <h2>Explore</h2>
+      <h4>Lose yourself in an amazing, evolving world</h4>
+      <button>START EXPLORING</button>
+    </p>
+    <p>
+      <h2>Create</h2>
+      <h4>Test the limits of your imagination</h4>
+      <button>START BUILDING</button>
+      <button>START DEVELOPING</button>
+    </p>
+
+    <p>
+      <h2>Trade</h2>
+      <h4>The virtual destination for digital assets</h4>
+      <button>START BROWSING</button>
     </p>
   </>

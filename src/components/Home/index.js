8dfb68c8f8565b8d52a339ee3c70b745cdbfa7bc
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-18 19:37:43 -0600

    basic navbar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from 'react';
+import '../../assets/css/home.css';
 
 export default () => 
   <>
-    <h1>Webaverse</h1>
-    <div>
-      <h2>Explore</h2>
-      <h4>Lose yourself in an amazing, evolving world</h4>
-      <button>START EXPLORING</button>
+    <div className="hero">
+      <div className="hero-copy">
+        <h1>Welcome to Webaverse</h1>
+        <p>Explore here</p>
+        <p>GET STARTED</p>
+      </div>
     </div>
     <div>
-      <h2>Create</h2>
-      <h4>Test the limits of your imagination</h4>
-      <button>START BUILDING</button>
-      <button>START DEVELOPING</button>
-    </div>
-    <div>
-      <h2>Trade</h2>
-      <h4>The virtual destination for digital assets</h4>
-      <button>START BROWSING</button>
+      <h1>Webaverse</h1>
+      <div>
+        <h2>Explore</h2>
+        <h4>Lose yourself in an amazing, evolving world</h4>
+        <button>START EXPLORING</button>
+      </div>
+      <div>
+        <h2>Create</h2>
+        <h4>Test the limits of your imagination</h4>
+        <button>START BUILDING</button>
+        <button>START DEVELOPING</button>
+      </div>
+      <div>
+        <h2>Trade</h2>
+        <h4>The virtual destination for digital assets</h4>
+        <button>START BROWSING</button>
+      </div>
     </div>
   </>
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 export default () => 
-  <h1>
-    <a href="/">Home</a>,{' '} 
-    <a href="/creators">Creators</a>,{' '} 
-    <a href="/account">My Account</a>
-  </h1>
+  <div className="navbar">
+    <h1>
+      <a href="/">Home</a>,{' '} 
+      <a href="/creators">Creators</a>,{' '} 
+      <a href="/account">My Account</a>
+    </h1>
+  </div>

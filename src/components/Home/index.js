6672631086bbbf0d3a282def2f8c69e805b06b17
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-19 02:21:46 -0600

    fix secondary hero sizing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -29,7 +29,7 @@ const SecondaryHero = ({heroBg, headTitle, title, subtitle, callToAction, ctaUrl
         backgroundSize: "cover",
       }}
     />
-    <div className="hero">
+    <div className="hero-secondary">
       <div className="hero-copy">
         <h1 className="head-title">{headTitle}</h1>
         <h1 className="secondary">{title}</h1>
@@ -43,19 +43,35 @@ const SecondaryHero = ({heroBg, headTitle, title, subtitle, callToAction, ctaUrl
 
 export default () => 
   <>
-    <Hero 
+    <Hero
       heroBg={logo}
       title="Webaverse"
       subtitle="An open virtual world built with existing communities."
       callToAction="GET STARTED"
       ctaUrl="https://docs.webaverse.com"
     />
-    <SecondaryHero 
+    <SecondaryHero
       heroBg=""
       headTitle="Explore"
-      title="Lose yourself in the music, the moment"
-      subtitle="Look. If you had one shot, or one opportunity, to sieze everything you ever wanted."
+      title="Lose yourself in an expansive, evolving world"
+      subtitle="Explore the Webaverse with your friends or go on a solo adventure."
       callToAction="START EXPLORING"
-      ctaUrl="https://docs.webaverse.com"
+      ctaUrl="https://app.webaverse.com/edit.html"
+    />
+    <SecondaryHero
+      heroBg=""
+      headTitle="Create"
+      title="Express yourself with your full capabilities"
+      subtitle="Creators from all backgrounds are able to create in Webaverse."
+      callToAction="START CREATING"
+      ctaUrl="https://docs.webaverse.com/docs/create/overview"
+    />
+    <SecondaryHero
+      heroBg=""
+      headTitle="Market"
+      title="Digital assets from the best creators"
+      subtitle="Buy, sell, trade your creations. Owners get micropayment as people use their items or avatars in Webaverse."
+      callToAction="START BROWSING"
+      ctaUrl="/browse"
     />
   </>

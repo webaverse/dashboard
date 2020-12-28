4750a2979bf2276487cf6af2f1c20cb1048d2c24
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-27 22:27:28 -0600

    fix wonky loader width

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import MoonLoader from "react-spinners/MoonLoader";
 
-export default ({ loading }) => loading ? <MoonLoader css={"display: inline-block"} size={50} color={"#c4005d"} /> : null
+export default ({ loading }) => loading ? <MoonLoader css={"display: inline-block; max-width: 200%;"} size={50} color={"#c4005d"} /> : null

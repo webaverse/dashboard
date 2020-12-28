f241ab5b4b63b7c02b2155215ded60fe2d3a912b
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-27 22:28:50 -0600

    add inventory owner options

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -1,183 +1,150 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Row, Col } from 'react-grid-system';
-import { useAppContext } from "../../libs/contextLib";
 import { setAvatar, setHomespace, setLoadoutState } from "../../functions/AssetFunctions";
 
-export default ({ inventory }) => {
+export default ({ globalState, setGlobalState, inventory, loadout }) => {
   if (!inventory) { return null; }
 
-  const { globalState, setGlobalState } = useAppContext();
+  console.log("loadout", loadout);
 
-  if (typeof inventory === 'object') {
-    const inventoryKeys = Object.keys(inventory);
-
-      console.log("globalState", globalState);
-    return inventoryKeys.map((item, i) => {
-      let url, name;
-      const address = item;
-      item = inventory[item];
-      const image = item.image || item.avatarPreview;
+  return inventory.map((item, i) => {
+    console.log("item", item);
+    let url, name;
+    const image = item.image || item.avatarPreview;
+    console.log("image", image);
   
-      if (!image || !item.name) { return; } // blank card
-      
-      if (item.id) {
-        url = "/browse/" + item.id;
-        name = item.name.replace(/\.[^/\\.]+$/, "");
-      } else if (item.address) {
-        url = "/accounts/" + address;
-        name = item.name ? item.name : "Anonymous";
-      }
+    if (!image) { return; } // blank card
 
-      const OwnerOptions = () => {
-        const wrapperRef = useRef(null);
-        const [isVisible, setIsVisible] = useState(false);
+    if (item.id) {
+      url = "/browse/" + item.id;
+      name = item.name;
+    } else if (item.address) {
+      url = "/accounts/" + item.address;
+      name = item.name ? item.name : "Anonymous";
+    }
 
-        useEffect(() => {
-          document.addEventListener("click", handleClickOutside, false);
-          return () => {
-            document.removeEventListener("click", handleClickOutside, false);
-          };
-        }, []);
+    const OwnerOptions = () => {
+      const wrapperRef = useRef(null);
+      const [isVisible, setIsVisible] = useState(false);
 
-        const handleClickOutside = event => {
-          if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-            setIsVisible(false);
-          }
+      useEffect(() => {
+        document.addEventListener("click", handleClickOutside, false);
+        return () => {
+          document.removeEventListener("click", handleClickOutside, false);
         };
+      }, []);
 
-        const AvatarOption = ({ item }) => {
-          let equal = false;
-          if (item) {
-            console.log("item.preview", item.image);
-            console.log("globalState.avatarPreview", globalState.avatarPreview);
-            equal = item.image === globalState.avatarPreview;
-          }
+      const handleSuccess = () => {
+        console.log("success");
+      };
 
-          return (
-            equal ?
-              <div className="popoutMenuOptions" onClick={(e) => { e.preventDefault(); alert(`test avatar ${item.id}`) }}>
-                Remove from avatar
-              </div >
-            :
-              <div className="popoutMenuOptions" onClick={(e) => { e.preventDefault(); alert(`test avatar ${item.id}`) }}>
-                Set as avatar
-              </div >
-          )
+      const handleError = (err) => {
+        console.error("error", err);
+      };
+
+      const handleClickOutside = event => {
+        if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+          setIsVisible(false);
         }
+      };
 
-        const HomeSpaceOption = ({ item }) => {
-          let equal = false;
-          if (item) {
-            console.log("item.preview", item.image);
-            console.log("globalState.homeSpacePreview", globalState.homeSpacePreview);
-            equal = item.image === globalState.homeSpacePreview;
-          }
-
-          return (
-            equal ?
-              <div className="popoutMenuOptions" onClick={(e) => { e.preventDefault(); alert(`test avatar ${item.id}`) }}>
-                Remove from homespace
-              </div >
-            :
-              <div className="popoutMenuOptions" onClick={(e) => { e.preventDefault(); alert(`test avatar ${item.id}`) }}>
-                Set as homespace
-              </div >
-          )
+      const AvatarOption = ({ item }) => {
+        let equal = false;
+        if (item) {
+          console.log("item.preview", item.image);
+          console.log("globalState.avatarPreview", globalState.avatarPreview);
+          equal = item.image === globalState.avatarPreview;
         }
-
-        const AddToLayoutOptions = ({ item }) => [1,2,3,4,5,6,7,8].map((num, i) => {
-          let equal = false;
-          const parsedLoadout = JSON.parse(globalState.loadout);
-          if (item && item.properties && parsedLoadout[num]) {
-            equal = item.properties.hash === "0x" + parsedLoadout[num][1].replace(/\.[^/\\.]+$/, "");
-          }
-
-          return (
-            equal ?
-              <div key={i} className="popoutMenuOptions" onClick={async (e) => {
-                e.preventDefault();
-                const newState = await setLoadoutState(item.id, num, globalState);
-                console.log("newState", newState);
-                setGlobalState(newState);
-              }}>
-               Remove from loadout {num}
-              </div>
-            :
-              <div key={i} className="popoutMenuOptions" onClick={async (e) => {
-                e.preventDefault();
-                const newState = await setLoadoutState(item.id, num, globalState);
-                console.log("newState", newState);
-                setGlobalState(newState);
-              }}>
-               Add to loadout {num}
-              </div>
-          )
-        });
 
         return (
-          <>
-            <div ref={wrapperRef} onClick={(e) => { e.preventDefault(); setIsVisible(!isVisible); }} className="tripleDot"></div>
-            { isVisible ?
-              <div className="popoutMenu">
-                <AvatarOption item={item} />
-                <HomeSpaceOption item={item} />
-                <AddToLayoutOptions item={item} />
-              </div>
-            : null }
-          </>
+          !equal && (
+            <div key={i} className="popoutMenuOptions" onClick={async (e) => {
+              e.preventDefault();
+              const newState = await setAvatar(item.id, globalState, handleSuccess, handleError);
+              setGlobalState(newState);
+            }}>
+              Set as avatar
+            </div >
+          )
         )
       }
-  
-      return (
-        <Col key={i} className="content" sm={12} md={3} style={{
-          backgroundImage: `url("${image}")`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-        }}>
-          <Link to={url}>
-            <div className="content-inner">
-              <h3 className="contentText">{name}</h3>
-              { item.owner && item.owner.address.toLowerCase() === globalState.address ?
-                <OwnerOptions />
-              :
-                null
-              }
-            </div>
-          </Link>
-        </Col>
-      )
-    })
-  } else {
-    return inventory.map((item, i) => {
-      let url, name;
-      const image = item.image || item.avatarPreview;
-  
-      if (!image || !item.name) { return; } // blank card
-      
-      if (item.id) {
-        url = "/browse/" + item.id;
-        name = item.name.replace(/\.[^/\\.]+$/, "");
-      } else if (item.address) {
-        url = "/accounts/" + item.address;
-        name = item.name ? item.name : "Anonymous";
+
+      const HomeSpaceOption = ({ item }) => {
+        let equal = false;
+        if (item) {
+          console.log("item.preview", item.image);
+          console.log("globalState.homeSpacePreview", globalState.homeSpacePreview);
+          equal = item.image === globalState.homeSpacePreview;
+        }
+
+        return (
+          !equal && (
+            <div key={i} className="popoutMenuOptions" onClick={async (e) => {
+              e.preventDefault();
+              const newState = await setHomespace(item.id, globalState, handleSuccess, handleError);
+              setGlobalState(newState);
+            }}>
+              Set as homespace
+            </div >
+          )
+        )
       }
-  
-      return (
-        <Col key={i} className="content" sm={12} md={4} style={{
-          backgroundImage: `url("${image}")`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-        }}>
-          <Link to={url}>
-            <div className="content-inner">
-              <h3 className="contentText">{name}</h3>
+
+      const AddToLayoutOptions = ({ item, loadout }) => [1,2,3,4,5,6,7,8].map((num, i) => {
+        let equal = false;
+        console.log("loadout:", loadout);
+        console.log("item:", item);
+        if (item && item.id && loadout && loadout[num]) {
+          equal = item.id == loadout[num-1][0];
+          console.log(equal);
+        }
+
+        return (
+          !equal && (
+            <div key={i} className="popoutMenuOptions" onClick={async (e) => {
+              e.preventDefault();
+              const newState = await setLoadoutState(item.id, num, globalState);
+              setGlobalState(newState);
+            }}>
+             Add to loadout {num}
             </div>
-          </Link>
-        </Col>
+          )
+        )
+      });
+
+      return (
+        <>
+          <div ref={wrapperRef} onClick={(e) => { e.preventDefault(); setIsVisible(!isVisible); }} className="tripleDot"></div>
+          { isVisible ?
+            <div className="popoutMenu">
+              <AvatarOption item={item} />
+              <HomeSpaceOption item={item} />
+              <AddToLayoutOptions loadout={loadout} item={item} />
+            </div>
+          : null }
+        </>
       )
-    })
-  }
+    }
+
+    return (
+      <Col key={i} className="content" sm={12} md={3} style={{
+        backgroundImage: `url("${image}")`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+      }}>
+        <Link to={url}>
+          <div className="content-inner">
+            <h3 className="contentText">{name}</h3>
+            { item.owner && globalState && item.owner.address.toLowerCase() === globalState.address ?
+              <OwnerOptions />
+            :
+              null
+            }
+          </div>
+        </Link>
+      </Col>
+    )
+  })
 }

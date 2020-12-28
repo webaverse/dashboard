b4103f2fbde3b4636d85dc60fa83bd8b23e2bc4d
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-27 22:23:36 -0600

    add metamask waiting for transfer, item price, and reorganize per item page

--- src/routes/BrowseInfo/index.js
+++ src/routes/BrowseInfo/index.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row, Col } from 'react-grid-system';
 import { Link, useParams } from "react-router-dom"
 import { useAppContext } from "../../libs/contextLib";
-import { getInventoryForCreator, getProfileForCreator } from "../../functions/UIStateFunctions.js";
+import { getStores, getInventoryForCreator, getProfileForCreator } from "../../functions/UIStateFunctions.js";
 import { buyAsset, sellAsset, depositAsset } from "../../functions/AssetFunctions.js";
 import Loader from "../../components/Loader";
+import "./style.css";
 
 export default () => {
   const { id } = useParams();
@@ -14,13 +15,48 @@ export default () => {
   const [profile, setProfile] = useState(null);
   const [sellPrice, setSellPrice] = useState(0);
   const [mainnetAddress, setMainnetAddress] = useState("");
+  const [store, setStore] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [pending, setPending] = useState(null);
 
   useEffect(() => {
     getInventoryForCreator(id, 0, true, globalState).then(res => {
       setItem(res.creatorInventories[id][0]);
       setLoading(false);
     });
+    getStores().then(res => {
+      if (res[id]) {
+        setStore(res[id].id);
+        setPrice(res[id].price);
+      }
+    });
   }, []);
+
+  const ethEnabled = () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+      return true;
+    }
+    return false;
+  }
+
+  const loginWithMetaMask = async () => {
+    if (!ethEnabled()) {
+      return "Please install MetaMask to use Webaverse!";
+    } else {
+      const web3 = window.web3;
+      const eth = await window.ethereum.request({ method: 'eth_accounts' });
+      if (eth && eth[0]) {
+        return eth[0];
+      } else {
+        ethereum.on('accountsChanged', (accounts) => {
+          handleTransfer();
+        });
+        return false;
+      }
+    }
+  }
 
   const handleSuccess = (res) => {
     console.log("HANDLING SUCCESS", res);
@@ -36,50 +72,49 @@ export default () => {
   const handleBuy = (e) => {
     e.preventDefault();
     setLoading(true);
-    console.log(globalState);
-    buyAsset(globalState.stores[id], 'sidechain', globalState.loginToken.mnemonic, handleSuccess, handleError);
+    getStores().then(res => {
+      buyAsset(res[id].id, 'sidechain', globalState.loginToken.mnemonic, handleSuccess, handleError);
+    });
   }
 
   const handleSell = (e) => {
     e.preventDefault();
     setLoading(true);
-    console.log(globalState);
     sellAsset(id, sellPrice, 'sidechain', globalState.loginToken.mnemonic, handleSuccess, handleError);
   }
 
   const handleTransfer = async (e) => {
-    e.preventDefault();
+    if(e) {
+      e.preventDefault();
+    }
+
     setLoading(true);
-    console.log(globalState);
-    await depositAsset(id, 'webaverse', mainnetAddress, globalState);
-    handleSuccess();
+
+    try {
+      const ethAccount = await loginWithMetaMask();
+      if (ethAccount) {
+        console.log(id, 'webaverse', mainnetAddress, globalState);
+        await depositAsset(id, 'webaverse', mainnetAddress, mainnetAddress, globalState);
+        handleSuccess();
+      } if (ethEnabled()) {
+        setPending(true);
+      } else {
+        setLoading(false);
+      }
+    } catch (err) {
+      handleError(err.toString());
+    }
   }
 
   const handleSellPriceChange = (e) => {
-    console.log(e.target.value);
     setSellPrice(e.target.value);
   }
   const handleMainnetAddressChange = (e) => {
-    console.log(e.target.value);
     setMainnetAddress(e.target.value);
   }
 
   const Buttons = () => {
-    console.log(globalState)
-    if (globalState.address && globalState.creatorInventories[id][0].owner.address.toLowerCase() == globalState.address) {
-      return (
-        <>
-          <input type="number" onChange={handleSellPriceChange} />
-          <a className="button" onClick={e => handleSell(e)}>
-            Sell
-          </a>
-          <input type="text" onChange={handleMainnetAddressChange} />
-          <a className="button" onClick={e => handleTransfer(e)}>
-            Transfer To Mainnet
-          </a>
-        </>
-      );
-    } else if (globalState.address) {
+    if (item && globalState.address && globalState.address != item.owner.address.toLowerCase()) {
       return (
         <>
           <a className="button" onClick={e => handleBuy(e)}>
@@ -97,17 +132,22 @@ export default () => {
       <Col sm={12} md={6}>
         <div className="profileHeader">
           <div className="profileName">
-            <h1>{item.name}</h1>
-            <Link to={"/accounts/" + item.owner.address.toLowerCase()}>
-              <p>Owner: {item.owner.username}</p>
+            <h1 className="itemText">{item.name}</h1>
+            <Link to={"/accounts/" + item.minter.address.toLowerCase()}>
+              <p className="itemText">Minter: {item.minter.username}</p>
             </Link>
             <p>Total Supply: {item.totalSupply}</p>
-            <Buttons />
+            { price && ( <p>price: {price}</p> )}
           </div>
         </div>
       </Col>
       <Col sm={12} md={6}>
-        <img className="itemImage" src={item.image} />
+        <img className="itemImage" style={{
+          backgroundImage: `url("${item.image}")`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+        }} />
       </Col>
     </>
   : null
@@ -115,8 +155,28 @@ export default () => {
   return (
     <Container>
       <Row style={{ justifyContent: "center" }}>
-        <Loader loading={loading} />
-        <Item />
+        { loading ?
+          <Loader loading={loading} />
+        : [
+          <Item />,
+          globalState.address && item && item.owner.address.toLowerCase() == globalState.address && (
+            <Row style={{ justifyContent: "center" }}>
+              <Col sm={12} md={8}>
+                <input className="infoInput" type="number" onChange={handleSellPriceChange} placeholder="Sale price" />
+                <a className="button" onClick={e => handleSell(e)}>
+                  Sell
+                </a>
+              </Col>
+              <Col sm={12} md={8}>
+                <input className="infoInput" type="text" onChange={handleMainnetAddressChange} placeholder="Mainnet address" />
+                <a className="button" onClick={e => handleTransfer(e)}>
+                  Transfer
+                </a>
+              </Col>
+            </Row>
+          ),
+          store && ( <Buttons /> )
+        ]}
       </Row>
     </Container>
   )

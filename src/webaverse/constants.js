cf2ce767a2d32aaeed4ea9078f44f44212753f2b
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-28 03:25:53 -0600

    update discord oauth url

--- src/webaverse/constants.js
+++ src/webaverse/constants.js
@@ -25,7 +25,7 @@ export const PLANET_OBJECT_SIZE = (
   Float32Array.BYTES_PER_ELEMENT * 4 // build.quaternion
 );
 
-export const discordOauthUrl = `https://discord.com/api/oauth2/authorize?client_id=684141574808272937&redirect_uri=https%3A%2F%2Fapp.webaverse.com%2Fdiscordlogin.html&response_type=code&scope=identify`;
+export const discordOauthUrl = `https://discord.com/api/oauth2/authorize?client_id=684141574808272937&redirect_uri=https%3A%2F%2Fwebaverse.com%2Flogin&response_type=code&scope=identify`;
 
 export const colors = [
   'ef5350',

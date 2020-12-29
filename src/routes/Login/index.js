a640f25e2e31134cebca8e83c7291a453664aa05
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-29 06:58:49 -0600

    dont redirect on login, add message

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom"
 import { useAppContext } from "../../libs/contextLib";
 import { parseQuery } from "../../functions/Functions";
@@ -9,26 +9,35 @@ export default () => {
   const code = new URLSearchParams(window.location.search).get("code") || "";
   const id = new URLSearchParams(window.location.search).get("id") || "";
   const { globalState, setGlobalState } = useAppContext();
+  const [message, setMessage] = useState("");
 
   useEffect(async () => {
-    if (code) {
-      const res = await fetch(`https://login.exokit.org/?discordcode=${code}&id=${id}`, {method: 'POST'});
+    if (code || login) {
+      const res = await fetch(`https://login.exokit.org/?discordcode=${code}&discordid=${id}`, {method: 'POST'});
       const j = await res.json();
       const {mnemonic} = j;
       if (mnemonic) {
         await storage.set('loginToken', { mnemonic });
         await storage.set('globalState', {...globalState, loginToken: { mnemonic } });
-        location.href = '/settings';
+//        location.href = '/settings';
         await setGlobalState({...globalState, loginToken: { mnemonic } });
+        setMessage('got mnemonic, logging in');
       } else {
-        console.warn('no mnemonic returned');
-        location.href = '/settings';
+        console.warn('no mnemonic returned from api');
+        setMessage('no mnemonic returned from api: ', j);
+//        location.href = '/settings';
       }
     } else {
-      console.warn('no discord code provided');
-      location.href = '/settings';
+      console.warn('no discord code or id provided');
+      setMessage('no discord code or id provided');
+//      location.href = '/settings';
     }
   }, []);
 
-  return <Loader loading={true} />
+  return loading ?
+    <Loader loading={true} />
+  :
+    <div>
+      <p>{message}</p>
+    </div>
 }

df089645660fe73d75ca251178041eeb35591402
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-30 03:15:29 -0600

    login correctly on login route

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from "react-router-dom"
+import { Redirect, useParams } from "react-router-dom"
 import { useAppContext } from "../../libs/contextLib";
 import { parseQuery } from "../../functions/Functions";
 import storage from "../../functions/Storage";
+import { loginWithPrivateKey, pullUser, getBalance } from "../../functions/UIStateFunctions.js";
+import bip39 from '../../libs/bip39.js';
 import Loader from "../../components/Loader";
 
 export default () => {
@@ -10,15 +12,34 @@ export default () => {
   const id = new URLSearchParams(window.location.search).get("id") || "";
   const { globalState, setGlobalState } = useAppContext();
 
+  const loginWithKey = (key) => {
+    if (bip39.validateMnemonic(key)) {
+      loginWithPrivateKey(key, globalState)
+      .then(res => {
+        setInitialState(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    } else {
+      alert("not a valid private key!");
+    }
+  }
+
+  const setInitialState = async (state) => {
+    const balance = await getBalance(state.address);
+    const newState = await pullUser(state);
+
+    setGlobalState({ balance, login: "true", ...newState });
+  }
+
   useEffect(() => {
     (async () => {
       const res = await fetch(`https://login.exokit.org/?discordcode=${code}&discordid=${id}`, {method: 'POST'});
       const j = await res.json();
       const {mnemonic} = j;
       if (mnemonic) {
-        await setGlobalState({...globalState, loginToken: { mnemonic } });
-        await storage.set("globalState", {...globalState, loginToken: { mnemonic } });
-        location.href = '/save';
+        loginWithKey(mnemonic);
       } else {
         console.warn('no mnemonic returned from api');
       }
@@ -28,6 +49,9 @@ export default () => {
   return (
     <div>
       <Loader loading={true} />
+      {globalState.address && (
+        <Redirect to={"/accounts/" + globalState.address} />
+      )}
     </div>
   )
 }

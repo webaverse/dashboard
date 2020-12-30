bf3984c9a1f6eb3ca900c3c9065e0b90330c86fe
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-30 07:59:35 -0600

    clean up routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Redirect, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Browse from './Browse';
 import BrowseInfo from './BrowseInfo';
@@ -12,23 +12,21 @@ import Home from './Home';
 import NotFound from './NotFound';
 
 export default () => 
-  <>
-    <Switch>
-      <Route exact path="/discord" component={() => {
-        window.location.href = "https://discord.gg/R5wqYhvv53";
-        return null;
-      }}/>
-      <Route exact path="/discordlogin" component={() => {
-        window.location.href = "https://discord.com/api/oauth2/authorize?client_id=684141574808272937&redirect_uri=https%3A%2F%2Fwebaverse.com%2Flogin&response_type=code&scope=identify";
-        return null;
-      }}/>
-      <Route path='/accounts/:id' component={() => <Accounts />} />
-      <Route path='/creators' component={() => <Creators />} />
-      <Route path='/browse' component={() => <Browse />} />
-      <Route path='/mint' component={() => <Mint />} />
-      <Route path='/login' component={() => <Login />} />
-      <Route path='/settings' component={() => <Settings />} />
-      <Route path='/' exact component={() => <Home />} />
-      <Route path='/*' component={() => <NotFound />} />
-    </Switch>
-  </>
+  <Switch>
+    <Route exact path="/discord" component={() => {
+      window.location.href = "https://discord.gg/R5wqYhvv53";
+      return null;
+    }}/>
+    <Route exact path="/discordlogin" component={() => {
+      window.location.href = "https://discord.com/api/oauth2/authorize?client_id=684141574808272937&redirect_uri=https%3A%2F%2Fwebaverse.com%2Flogin&response_type=code&scope=identify";
+      return null;
+    }}/>
+    <Route path='/accounts/:id' component={() => <Accounts />} />
+    <Route path='/creators' component={() => <Creators />} />
+    <Route path='/browse' component={() => <Browse />} />
+    <Route path='/mint' component={() => <Mint />} />
+    <Route path='/login' component={() => <Login />} />
+    <Route path='/settings' component={() => <Settings />} />
+    <Route path='/' exact component={() => <Home />} />
+    <Route path='/*' component={() => <NotFound />} />
+  </Switch>

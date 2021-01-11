016fc991f0da7dae9ff5b3e2b7a7e296697ae09b
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-11 08:46:25 -0600

    add 404 page design fix #387

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,12 @@
-
+import Image from 'next/image'
 
 export default function FourOhFour() {
 
   return (
     <>
-      <h1>404</h1>
-      <p>
-        Not found
-      </p>
+      <h1 style={{ fontSize: "3rem" }}>404</h1>
+      <h2>Page not found.</h2>
+      <Image src="/404.png" width={121} height={459} />
     </>
   )
 }

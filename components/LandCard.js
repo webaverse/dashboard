aecdf6e165ca64a52abb23d069d68bd9cc97a9c1
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-11 12:45:12 -0600

    fix card styling

--- components/LandCard.js
+++ components/LandCard.js
@@ -78,7 +78,7 @@ export default ({
         </div>
         <div className={`assetImage assetImage ${cardSize}`}><img src={image} /></div>
         <div className={`lowerCardInfo lowerCardInfo ${cardSize}`}>
-          <div className={`lowerCardInfoTop ${rarity} lowerCardInfoTop ${cardSize} lowerCardInfoTop`}>
+          <div className={`lowerLandCardInfoTop ${rarity} lowerCardInfoTop ${cardSize} lowerCardInfoTop`}>
             <div className={`lowerCardInfoTopLeft lowerCardInfoTopLeft ${cardSize}`}>
               <div className="lowerCardInfoTopClear"></div>
             </div>

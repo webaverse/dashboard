d07b808374c0643b5b905459fdacc912ddfe0762
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-14 09:30:17 -0600

    update tilemap used in map component

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { MapContainer, CircleMarker, Tooltip, ImageOverlay } from 'react-leaflet';
+import { MapContainer, CircleMarker, Tooltip, TileLayer } from 'react-leaflet';
 import L from "leaflet";
 
 export default () => {
-  const bounds = [[0,0], [808,804]];
+  const bounds = [[0,0], [2,2]];
 
   return ( 
-    <MapContainer className="mapContainer" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-      <ImageOverlay
-        bounds={bounds}
-        url="https://user-images.githubusercontent.com/29695350/104216796-260c3700-5400-11eb-84cb-98b88bb8a7f6.PNG"
+    <MapContainer className="mapContainer" center={[0, 0]} zoom={3} scrollWheelZoom={true}>
+      <TileLayer
+        url="https://land-preview.exokit.org/{z}/{x}/{y}"
       />
     </MapContainer>
   )

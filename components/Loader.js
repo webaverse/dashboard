1f721386e8f5ee1208db4485fb0d4a80b2e4b5f5
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-14 12:21:36 -0600

    fix loader styling

--- components/Loader.js
+++ components/Loader.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import MoonLoader from "react-spinners/MoonLoader";
 
-export default ({ loading }) => loading ? <MoonLoader css={"display: inline-block; max-width: 200%;"} size={50} color={"#c4005d"} /> : null
+export default ({ loading }) => loading ?
+  <div style={{
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }}>
+    <MoonLoader css={{ display: "flex" }} size={50} color={"#c4005d"} />
+  </div>
+: null

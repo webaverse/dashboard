07a8cd8f5f6e63c74280a535bba39581a91a6a4f
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-14 14:46:29 -0600

    add map route

---
+++ pages/map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="mapContainer">
+      <iframe className="mapFrame" src="https://map.webaverse.com" />
+    </div>
+  )
+}

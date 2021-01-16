150c64f600c713f1d37735a6a829cf42eecfe52c
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-16 12:21:07 -0600

    check for correct network on carddetails

--- components/CardDetails.js
+++ components/CardDetails.js
@@ -72,14 +72,20 @@ export default ({
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
       window.ethereum.enable();
-      return true;
+      if (window.web3.version.network == 4) {
+        return true;
+      } else {
+        alert("You need to be on the Rinkeby network.");
+        return false;
+      }
     }
+    alert("Please install MetaMask to use Webaverse!");
     return false;
   }
 
   const loginWithMetaMask = async () => {
     if (!ethEnabled()) {
-      return "Please install MetaMask to use Webaverse!";
+      return;
     } else {
       const web3 = window.web3;
       try {
@@ -181,8 +187,6 @@ export default ({
         const mainnetAddress = prompt("What mainnet address do you want to send to?", "0x0");
         await depositAsset(id, 'webaverse', mainnetAddress, globalState.address, globalState);
         handleSuccess();
-      } if (ethEnabled()) {
-        setPending(true);
       } else {
         setLoading(false);
       }

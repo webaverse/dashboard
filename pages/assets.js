c8649a4085a13ec14596859688e43ea88d0a8525
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-16 09:14:23 -0600

    list all assets on browse and infinite scroll

--- pages/assets.js
+++ pages/assets.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { useAppContext } from "../libs/contextLib";
-import { getBooths } from "../functions/UIStateFunctions.js";
+import { getTokens } from "../functions/UIStateFunctions.js";
 import CardGrid from "../components/CardGrid";
 
 export default ({ data }) => {
+  const [hasMore, setHasMore] = useState(true);
+  const [start, setStart] = useState(51);
+  const [end, setEnd] = useState(60);
+  const [cardData, setCardData] = useState(data);
   const { globalState, setGlobalState } = useAppContext();
 
-  return <CardGrid data={data} globalState={globalState} />
+  const fetchData = async () => {
+    const newData = await getTokens(start, end);
+
+    if (newData[newData.length-1].minter.address === "0x0000000000000000000000000000000000000000") {
+      setHasMore(false);
+      return;
+    }
+
+    setCardData(cardData.concat(newData));
+    setStart(start+20);
+    setEnd(end+20);
+  }
+
+  return (
+    <div className="container">
+      <InfiniteScroll
+        dataLength={cardData.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={hasMore}
+        loader={<p className="containerText">Loading...</p>}
+        endMessage={
+          <p className="containerText">
+            You have seen it all!
+          </p>
+        }
+      >
+        <CardGrid data={cardData} cardSize="small" globalState={globalState} />
+      </InfiniteScroll>
+    </div>
+  )
 }
 
 export async function getServerSideProps() {
-  const data = await getBooths(0);
+  const data = await getTokens(1, 50);
 
   return { props: { data } }
 }

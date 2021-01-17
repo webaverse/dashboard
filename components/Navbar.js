f07bdfcef57ec86d1bc6fdd50ee80f773f42af8a
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-16 19:18:21 -0600

    fix navbar spacing

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,38 +10,38 @@ const Navbar = () => {
     <div className={styles.navbar}>
       <div className={styles.container}>
         <div className={styles.navbarMenu}>
-            <div className={styles.leftMenuContainer}>
-              <Link href="/">
-                <img className="logo" src="/webaverse.png" alt="Webaverse logo" />
+          <div className={styles.leftMenuContainer}>
+            <Link href="/">
+              <img className="logo" src="/webaverse.png" alt="Webaverse logo" />
+            </Link>
+          </div>
+          <div className={styles.secondaryMenu}>
+            <Link href="/">
+              Webaverse
+            </Link>
+          </div>
+          <div className={styles.rightMenuContainer}>
+            <Link href="/assets"><a className={styles.item}>Browse</a></Link>
+            <Link href="/land"><a className={styles.item}>Land</a></Link>
+            <Link href="/map"><a className={styles.item}>Map</a></Link>
+            <Link href="/accounts"><a className={styles.item}>Accounts</a></Link>
+            <Link href="/mint"><a className={styles.item}>Mint</a></Link>
+            <a className={styles.item} href="https://docs.webaverse.com">Docs</a>
+            <a className={styles.item} href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
+            <a className={styles.item} target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
+            <a className={styles.item} href="https:/app.webaverse.com">Play</a>
+          </div>
+          <div>
+            { globalState.address ?
+              <a href={"/accounts/" + globalState.address}>
+                <img className={`${styles.accountPicture} ${styles.loggedIn}`} src={globalState.avatarPreview ? globalState.avatarPreview.replace(/\.[^.]*$/, '.png') : "/preview.png"} />
+              </a>
+            :
+              <Link href="/login">
+                <img className="accountPicture" src="/preview.png" alt="Placeholder profile picture" />
               </Link>
-            </div>
-            <div className={styles.secondaryMenu}>
-              <Link href="/">
-                Webaverse
-              </Link>
-            </div>
-            <div className={styles.rightMenuContainer}>
-              <Link href="/assets"><a className={styles.item}>Browse</a></Link>
-              <Link href="/land"><a className={styles.item}>Land</a></Link>
-              <Link href="/map"><a className={styles.item}>Map</a></Link>
-              <Link href="/accounts"><a className={styles.item}>Accounts</a></Link>
-              <Link href="/mint"><a className={styles.item}>Mint</a></Link>
-              <a className={styles.item} href="https://docs.webaverse.com">Docs</a>
-              <a className={styles.item} href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
-              <a className={styles.item} target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
-              <a className={styles.item} href="https:/app.webaverse.com">Play</a>
-            </div>
-            <div>
-              { globalState.address ?
-                <a href={"/accounts/" + globalState.address}>
-                  <img className={`${styles.accountPicture} ${styles.loggedIn}`} src={globalState.avatarPreview ? globalState.avatarPreview.replace(/\.[^.]*$/, '.png') : "/preview.png"} />
-                </a>
-              :
-                <Link href="/login">
-                  <img className="accountPicture" src="/preview.png" alt="Placeholder profile picture" />
-                </Link>
-              }
-            </div>
+            }
+          </div>
         </div>
       </div>
     </div>

cf9c9527479c23343ce522364e27b1549cac5d6a
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-16 19:57:57 -0600

    remove module classnames in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,40 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link'
 import { useAppContext } from "../libs/contextLib";
-import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => {
   const { globalState, setGlobalState } = useAppContext();
 
   return (
-    <div className={styles.navbar}>
-      <div className={styles.container}>
-        <div className={styles.navbarMenu}>
-          <div className={styles.leftMenuContainer}>
+    <div className="navbar">
+      <div className="navbarContainer">
+        <div className="navbarMenu">
+          <div className="leftMenuContainer">
             <Link href="/">
               <img className="logo" src="/webaverse.png" alt="Webaverse logo" />
             </Link>
           </div>
-          <div className={styles.secondaryMenu}>
+          <div className="secondaryMenu">
             <Link href="/">
               Webaverse
             </Link>
           </div>
-          <div className={styles.rightMenuContainer}>
-            <Link href="/assets"><a className={styles.item}>Browse</a></Link>
-            <Link href="/land"><a className={styles.item}>Land</a></Link>
-            <Link href="/map"><a className={styles.item}>Map</a></Link>
-            <Link href="/accounts"><a className={styles.item}>Accounts</a></Link>
-            <Link href="/mint"><a className={styles.item}>Mint</a></Link>
-            <a className={styles.item} href="https://docs.webaverse.com">Docs</a>
-            <a className={styles.item} href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
-            <a className={styles.item} target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
-            <a className={styles.item} href="https:/app.webaverse.com">Play</a>
+          <div className="rightMenuContainer">
+            <Link href="/assets"><a className="item">Browse</a></Link>
+            <Link href="/land"><a className="item">Land</a></Link>
+            <Link href="/map"><a className="item">Map</a></Link>
+            <Link href="/accounts"><a className="item">Accounts</a></Link>
+            <Link href="/mint"><a className="item">Mint</a></Link>
+            <a className="item" href="https://docs.webaverse.com">Docs</a>
+            <a className="item" href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
+            <a className="item" target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
+            <a className="item" href="https:/app.webaverse.com">Play</a>
           </div>
           <div>
             { globalState.address ?
               <a href={"/accounts/" + globalState.address}>
-                <img className={`${styles.accountPicture} ${styles.loggedIn}`} src={globalState.avatarPreview ? globalState.avatarPreview.replace(/\.[^.]*$/, '.png') : "/preview.png"} />
+                <img className={`accountPicture loggedIn`} src={globalState.avatarPreview ? globalState.avatarPreview.replace(/\.[^.]*$/, '.png') : "/preview.png"} />
               </a>
             :
               <Link href="/login">

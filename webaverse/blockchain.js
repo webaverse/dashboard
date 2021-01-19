f1ad9172167ebadd64c64a46e9c293943ef18bbd
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-18 19:33:46 -0600

    dynmically import addresses/abis in blockchain

--- webaverse/blockchain.js
+++ webaverse/blockchain.js
@@ -6,50 +6,55 @@ import ethereumJsTx from '../libs/ethereumjs-tx.js';
 import {makePromise} from './util.js';
 import {storageHost, web3SidechainEndpoint} from './constants.js';
 const {Transaction, Common} = ethereumJsTx;
-import addresses from './address.js';
-import abis from './abi.js';
 
-let {
-  main: {Account: AccountAddress, FT: FTAddress, NFT: NFTAddress, FTProxy: FTProxyAddress, NFTProxy: NFTProxyAddress, Trade: TradeAddress, LAND: LANDAddress, LANDProxy: LANDProxyAddress },
-  sidechain: {Account: AccountAddressSidechain, FT: FTAddressSidechain, NFT: NFTAddressSidechain, FTProxy: FTProxyAddressSidechain, NFTProxy: NFTProxyAddressSidechain, Trade: TradeAddressSidechain, LAND: LANDAddressSidechain, LANDProxy: LANDProxyAddressSidechain },
-} = addresses;
-let {Account: AccountAbi, FT: FTAbi, FTProxy: FTProxyAbi, NFT: NFTAbi, NFTProxy: NFTProxyAbi, Trade: TradeAbi, LAND:LANDAbi, LANDProxy: LANDProxyAbi } = abis;
+const getWeb3OrContracts = async() => {
+  const  addresses = await fetch('https://contracts.webaverse.com/ethereum/address.js').then(res => res.text()).then(s => JSON.parse(s.replace(/^\s*export\s*default\s*/, '')));
 
-let main;
-if (typeof window !== 'undefined' && window.ethereum) {
-  main = new Web3(window.ethereum);
-} else {
-  main = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/420"));
+   const abis = await fetch('https://contracts.webaverse.com/ethereum/abi.js').then(res => res.text()).then(s => JSON.parse(s.replace(/^\s*export\s*default\s*/, '')));
+
+  let {
+    main: {Account: AccountAddress, FT: FTAddress, NFT: NFTAddress, FTProxy: FTProxyAddress, NFTProxy: NFTProxyAddress, Trade: TradeAddress, LAND: LANDAddress, LANDProxy: LANDProxyAddress },
+    sidechain: {Account: AccountAddressSidechain, FT: FTAddressSidechain, NFT: NFTAddressSidechain, FTProxy: FTProxyAddressSidechain, NFTProxy: NFTProxyAddressSidechain, Trade: TradeAddressSidechain, LAND: LANDAddressSidechain, LANDProxy: LANDProxyAddressSidechain },
+  } = addresses;
+  let {Account: AccountAbi, FT: FTAbi, FTProxy: FTProxyAbi, NFT: NFTAbi, NFTProxy: NFTProxyAbi, Trade: TradeAbi, LAND:LANDAbi, LANDProxy: LANDProxyAbi } = abis;
+
+  let main;
+  if (typeof window !== 'undefined' && window.ethereum) {
+    main = new Web3(window.ethereum);
+  } else {
+    main = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/420"));
+  }
+
+  const web3 = {
+    main: main,
+    sidechain: new Web3(new Web3.providers.HttpProvider(web3SidechainEndpoint)),
+  };
+  web3['sidechain'].eth.transactionConfirmationBlocks = 1;
+
+  const contracts = {
+    main: {
+      Account: new web3['main'].eth.Contract(AccountAbi, AccountAddress),
+      FT: new web3['main'].eth.Contract(FTAbi, FTAddress),
+      FTProxy: new web3['main'].eth.Contract(FTProxyAbi, FTProxyAddress),
+      NFT: new web3['main'].eth.Contract(NFTAbi, NFTAddress),
+      NFTProxy: new web3['main'].eth.Contract(NFTProxyAbi, NFTProxyAddress),
+      Trade: new web3['main'].eth.Contract(TradeAbi, TradeAddress),
+      LAND: new web3['main'].eth.Contract(LANDAbi, LANDAddress),
+      LANDProxy: new web3['main'].eth.Contract(LANDProxyAbi, LANDProxyAddress),
+    },
+    sidechain: {
+      Account: new web3['sidechain'].eth.Contract(AccountAbi, AccountAddressSidechain),
+      FT: new web3['sidechain'].eth.Contract(FTAbi, FTAddressSidechain),
+      FTProxy: new web3['sidechain'].eth.Contract(FTProxyAbi, FTProxyAddressSidechain),
+      NFT: new web3['sidechain'].eth.Contract(NFTAbi, NFTAddressSidechain),
+      NFTProxy: new web3['sidechain'].eth.Contract(NFTProxyAbi, NFTProxyAddressSidechain),
+      Trade: new web3['sidechain'].eth.Contract(TradeAbi, TradeAddressSidechain),
+      LAND: new web3['sidechain'].eth.Contract(LANDAbi, LANDAddressSidechain),
+      LANDProxy: new web3['sidechain'].eth.Contract(LANDProxyAbi, LANDProxyAddressSidechain),
+    },
+  };
+  return { web3, contracts, addresses };
 }
-
-const web3 = {
-  main: main,
-  sidechain: new Web3(new Web3.providers.HttpProvider(web3SidechainEndpoint)),
-};
-web3['sidechain'].eth.transactionConfirmationBlocks = 1;
-
-const contracts = {
-  main: {
-    Account: new web3['main'].eth.Contract(AccountAbi, AccountAddress),
-    FT: new web3['main'].eth.Contract(FTAbi, FTAddress),
-    FTProxy: new web3['main'].eth.Contract(FTProxyAbi, FTProxyAddress),
-    NFT: new web3['main'].eth.Contract(NFTAbi, NFTAddress),
-    NFTProxy: new web3['main'].eth.Contract(NFTProxyAbi, NFTProxyAddress),
-    Trade: new web3['main'].eth.Contract(TradeAbi, TradeAddress),
-    LAND: new web3['main'].eth.Contract(LANDAbi, LANDAddress),
-    LANDProxy: new web3['main'].eth.Contract(LANDProxyAbi, LANDProxyAddress),
-  },
-  sidechain: {
-    Account: new web3['sidechain'].eth.Contract(AccountAbi, AccountAddressSidechain),
-    FT: new web3['sidechain'].eth.Contract(FTAbi, FTAddressSidechain),
-    FTProxy: new web3['sidechain'].eth.Contract(FTProxyAbi, FTProxyAddressSidechain),
-    NFT: new web3['sidechain'].eth.Contract(NFTAbi, NFTAddressSidechain),
-    NFTProxy: new web3['sidechain'].eth.Contract(NFTProxyAbi, NFTProxyAddressSidechain),
-    Trade: new web3['sidechain'].eth.Contract(TradeAbi, TradeAddressSidechain),
-    LAND: new web3['sidechain'].eth.Contract(LANDAbi, LANDAddressSidechain),
-    LANDProxy: new web3['sidechain'].eth.Contract(LANDProxyAbi, LANDProxyAddressSidechain),
-  },
-};
 
 const transactionQueue = {
   running: false,
@@ -72,6 +77,7 @@ const transactionQueue = {
   },
 };
 const runSidechainTransaction = mnemonic => async (contractName, method, ...args) => {
+  const { web3, contracts } = await getWeb3OrContracts();
   const wallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic)).derivePath(`m/44'/60'/0'/0/0`).getWallet();
   const address = wallet.getAddressString();
   const privateKey = wallet.getPrivateKeyString();
@@ -111,9 +117,6 @@ const runSidechainTransaction = mnemonic => async (contractName, method, ...args
   return receipt;
 };
 const getTransactionSignature = async (chainName, contractName, transactionHash) => {
-  console.log("chainName", chainName);
-  console.log("contractName", contractName);
-  console.log("transactionHash", transactionHash);
   const u = `https://sign.exokit.org/${chainName}/${contractName}/${transactionHash}`;
   for (let i = 0; i < 10; i++) {
     const signature = await fetch(u).then(res => res.json());
@@ -136,8 +139,7 @@ const getAddressFromMnemonic = mnemonic => _getWalletFromMnemonic(mnemonic)
   .getAddressString();
 
 export {
-  web3,
-  contracts,
+  getWeb3OrContracts,
   runSidechainTransaction,
   getTransactionSignature,
   getAddressFromMnemonic,

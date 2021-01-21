4914767c3068839eec0c22511b138c9f590f48af
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-21 16:55:47 -0600

    organize submenus for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -21,45 +21,60 @@ const Navbar = () => {
               Webaverse
             </Link>
           </div>
-          <div onClick={() => setDropdown(false)} className={`rightMenuContainer ${dropdown ? "responsive" : "hidden"}`}>
-            <Link href="/assets"><a className="item">Browse</a></Link>
-            <Link href="/land"><a className="item">Land</a></Link>
-            <Link href="/map"><a className="item">Map</a></Link>
-            <Link href="/accounts"><a className="item">Accounts</a></Link>
-            <Link href="/mint"><a className="item">Mint</a></Link>
-            <a className="item" href="https://docs.webaverse.com">Docs</a>
-            <a className="item" href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
-            <a className="item" target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
-            <a className="item" href="https://app.webaverse.com">Play</a>
+          <div onClick={() => setDropdown(false)} className={`rightMenuContainer mobile ${dropdown ? "responsive" : ""}`}>
+            <Link href="/assets"><a className={`navbarSubMenuLinkItem item`}>Browse</a></Link>
+            <Link href="/land"><a className={`navbarSubMenuLinkItem item`}>Land</a></Link>
+            <Link href="/map"><a className={`navbarSubMenuLinkItem item`}>Map</a></Link>
+            <Link href="/accounts"><a className={`navbarSubMenuLinkItem item`}>Accounts</a></Link>
+            <Link href="/mint"><a className={`navbarSubMenuLinkItem item`}>Mint</a></Link>
+            <a className={`navbarSubMenuLinkItem item`} href="https://docs.webaverse.com">Docs</a>
+            <a className={`navbarSubMenuLinkItem item`} href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
+            <a className={`navbarSubMenuLinkItem item`} target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
+            <a className={`navbarSubMenuLinkItem item`} href="https://app.webaverse.com">Play</a>
           </div>
-          <div onClick={() => setDropdown(false)} className={`rightMenuContainer ${dropdown ? "hidden" : ""}`}>
+          <div onClick={() => setDropdown(false)} className={`rightMenuContainer desktop`}>
             <div className="navbarTopLevelMenuOption">
-              <a className="navbarLinkLearn item" href="https://docs.webaverse.com">Learn</a>
+              <a className="navbarLinkItem item" href="https://app.webaverse.com">Play</a>
+            </div>
+            <div className="navbarTopLevelMenuOption">
+              <a className="navbarLinkItem item" href="https://docs.webaverse.com">Learn</a>
               <div className="navbarSubMenuContainer">
                 <div className="navbarSubMenu">
                   <div className="navbarSubMenuLeftbar">
-                    <Link href="/assets"><a className="item">Browse</a></Link>
-                    <Link href="/land"><a className="item">Land</a></Link>
-                    <Link href="/map"><a className="item">Map</a></Link>
-                    <Link href="/accounts"><a className="item">Accounts</a></Link>
-                    <Link href="/mint"><a className="item">Mint</a></Link>
-                    <a className="item" href="https://docs.webaverse.com">Docs</a>
-                    <a className="item" href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
-                    <a className="item" target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
-                    <a className="item" href="https://app.webaverse.com">Play</a>
+                    <a className={`navbarSubMenuLinkItem item`} href="https://docs.webaverse.com">Docs</a>
+                    <a className={`navbarSubMenuLinkItem item`} href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>
+                    <a className={`navbarSubMenuLinkItem item`} target="_blank" href="https://discord.gg/3byWubumSa">Discord</a>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="navbarTopLevelMenuOption">
+              <Link href="/assets"><a className="navbarLinkItem item">Market</a></Link>
+              <div className="navbarSubMenuContainer">
+                <div className="navbarSubMenu">
+                  <div className="navbarSubMenuLeftbar">
+                    <Link href="/assets"><a className={`navbarSubMenuLinkItem item`}>Browse</a></Link>
+                    <Link href="/land"><a className={`navbarSubMenuLinkItem item`}>Land</a></Link>
+                    <Link href="/map"><a className={`navbarSubMenuLinkItem item`}>Map</a></Link>
+                    <Link href="/accounts"><a className={`navbarSubMenuLinkItem item`}>Accounts</a></Link>
+                    <Link href="/mint"><a className={`navbarSubMenuLinkItem item`}>Mint</a></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="navbarTopLevelMenuOption">
+              <Link href="/mint"><a className="navbarLinkItem item">Mint</a></Link>
+            </div>
           </div>
-          <div onClick={() => setDropdown(false)} className={`rightMenuContainer ${dropdown ? "hidden" : ""}`}>
+
+          <div onClick={() => setDropdown(false)} className={`navbarFluxContainer desktop`}>
             <a className="navbarFluxSymbol">
               山
             </a>
             <a className="navbarFluxAmount">
               {globalState && globalState.balance ? Number(globalState.balance).toLocaleString() : "0"}
             </a>
-            <div className={`navbarFluxPlusContainer noselect ${dropdown ? "responsive" : ""}`}>
+            <div className={`navbarFluxPlusContainer noselect`}>
               <a className="navbarFluxPlus">
                 +
               </a>

e00409c5f13dd4633a0bd6707b6b2a164e37b504
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-21 23:16:06 -0600

    land page useffect refactor

--- pages/land.js
+++ pages/land.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAppContext } from "../libs/contextLib";
 import { getLands } from "../functions/UIStateFunctions.js";
 import LandCardGrid from "../components/LandCardGrid";
+import Loader from "../components/Loader";
 
-export default ({ data }) => {
+export default () => {
   const { globalState, setGlobalState } = useAppContext();
+  const [lands, setLands] = useState(null);
 
-  return data && data.length > 0 &&
+  useEffect(() => {
+    (async () => {
+      const data = await getLands(1, 100);
+      setLands(data);
+    })();
+  }, []);
+
+  return (
     <div className="container">
-      <LandCardGrid data={data} globalState={globalState} />
+      { lands ?
+        <LandCardGrid data={lands} globalState={globalState} />
+      :
+        <Loader loading={true} />
+      }
     </div>
-}
-
-export async function getServerSideProps(context) {
-  const data = await getLands(1, 100, context.req.headers.host);
-
-  return { props: { data } }
+  )
 }

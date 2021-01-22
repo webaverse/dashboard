58dbfdf6e9fb04aea07eef461f299881e0fd0435
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-22 13:32:51 -0600

    update accounts to members

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
             <Link href="/assets"><a className={`item`}>Browse</a></Link>
             <Link href="/land"><a className={`item`}>Land</a></Link>
             <Link href="/map"><a className={`item`}>Map</a></Link>
-            <Link href="/accounts"><a className={`item`}>Accounts</a></Link>
+            <Link href="/accounts"><a className={`item`}>Members</a></Link>
             <Link href="/mint"><a className={`item`}>Mint</a></Link>
             <a className={`item`} href="https://docs.webaverse.com">Docs</a>
             <a className={`item`} href="https://webaverse.github.io/whitepaper/whitepaper.pdf">Whitepaper</a>

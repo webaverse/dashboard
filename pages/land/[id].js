69f598f45507da9c76550c30844630a18f4eaf23
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-21 23:15:53 -0600

    per land useeffect refactor

--- pages/land/[id].js
+++ pages/land/[id].js
@@ -1,6 +1,8 @@
+import React, { useState, useEffect } from "react";
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import LandCardDetails from "../../components/LandCardDetails";
+import Loader from "../../components/Loader";
 import { getLand } from "../../functions/UIStateFunctions";
 import { useAppContext } from "../../libs/contextLib";
 
@@ -8,44 +10,54 @@ export default ({ data }) => {
   const router = useRouter()
   const { id } = router.query
   const { globalState, setGlobalState } = useAppContext();
+  const [land, setLand] = useState(null);
+
+  useEffect(() => {
+    if (id && !land) {
+      (async () => {
+        const data = await getLand(id);
+        setLand(data);
+      })();
+    }
+  }, [id]);
 
   return (
     <>
-      <Head>
-        <title>{data.name} | Webaverse</title>
-        <meta name="description" content={data.description + " | Webaverse"} />
-        <meta property="og:title" content={data.name + " | Webaverse"} />
-        <meta property="og:image" content={data.image} />
-        <meta name="theme-color" content="#c4005d" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <LandCardDetails
-         id={data.id}
-         key={data.id}
-         name={data.name}
-         description={data.description}
-         image={data.image}
-         buyPrice={data.buyPrice}
-         storeId={data.storeId}
-         hash={data.properties.hash}
-         external_url={data.external_url}
-         filename={data.properties.filename}
-         rarity={data.properties.rarity}
-         ext={data.properties.ext}
-         totalSupply={data.totalSupply}
-         balance={data.balance}
-         ownerAvatarPreview={data.owner.avatarPreview}
-         ownerUsername={data.owner.username}
-         ownerAddress={data.owner.address}
-         globalState={globalState}
-         networkType='webaverse'
-       />
+      { land ?
+        <>
+          <Head>
+            <title>{land.name} | Webaverse</title>
+            <meta name="description" content={land.description + " | Webaverse"} />
+            <meta property="og:title" content={land.name + " | Webaverse"} />
+            <meta property="og:image" content={land.image} />
+            <meta name="theme-color" content="#c4005d" />
+            <meta name="twitter:card" content="summary_large_image" />
+          </Head>
+          <LandCardDetails
+             id={land.id}
+             key={land.id}
+             name={land.name}
+             description={land.description}
+             image={land.image}
+             buyPrice={land.buyPrice}
+             storeId={land.storeId}
+             hash={land.properties.hash}
+             external_url={land.external_url}
+             filename={land.properties.filename}
+             rarity={land.properties.rarity}
+             ext={land.properties.ext}
+             totalSupply={land.totalSupply}
+             balance={land.balance}
+             ownerAvatarPreview={land.owner.avatarPreview}
+             ownerUsername={land.owner.username}
+             ownerAddress={land.owner.address}
+             globalState={globalState}
+             networkType='webaverse'
+           />
+        </>
+      :
+        <Loader loading={true} />
+      }
     </>
   )
-}
-
-export async function getServerSideProps({ params }) {
-  const data = await getLand(params.id);
-
-  return { props: { data } }
 }

c3c91b60e0853fb7bc24e058659532f7b73e3629
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-21 23:14:52 -0600

    per asset page useffect refactor

--- pages/assets/[id].js
+++ pages/assets/[id].js
@@ -1,54 +1,65 @@
+import React, { useState, useEffect } from "react";
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import CardDetails from "../../components/CardDetails";
+import Loader from "../../components/Loader";
 import { getToken } from "../../functions/UIStateFunctions";
 import { useAppContext } from "../../libs/contextLib";
 
-
-export default ({ data }) => {
+export default () => {
   const router = useRouter()
   const { id } = router.query
   const { globalState, setGlobalState } = useAppContext();
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    if (id && !token) {
+      (async () => {
+        const data = await getToken(id);
+        setToken(data);
+      })();
+    }
+  }, [id]);
 
   return (
     <>
-      <Head>
-        <title>{data.name} | Webaverse</title>
-        <meta name="description" content={data.description + " | Webaverse"} />
-        <meta property="og:title" content={data.name + " | Webaverse"} />
-        <meta property="og:image" content={data.image} />
-        <meta name="theme-color" content="#c4005d" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <CardDetails
-         id={data.id}
-         key={data.id}
-         name={data.name}
-         description={data.description}
-         image={data.image}
-         buyPrice={data.buyPrice}
-         storeId={data.storeId}
-         hash={data.properties.hash}
-         external_url={data.external_url}
-         filename={data.properties.filename}
-         ext={data.properties.ext}
-         totalSupply={data.totalSupply}
-         balance={data.balance}
-         ownerAvatarPreview={data.owner.avatarPreview}
-         ownerUsername={data.owner.username}
-         ownerAddress={data.owner.address}
-         minterAvatarPreview={data.minter.avatarPreview}
-         minterAddress={data.minter.address}
-         minterUsername={data.minter.username}
-         globalState={globalState}
-         networkType='webaverse'
-       />
+      { token ?
+        <>
+          <Head>
+            <title>{token.name} | Webaverse</title>
+            <meta name="description" content={token.description + " | Webaverse"} />
+            <meta property="og:title" content={token.name + " | Webaverse"} />
+            <meta property="og:image" content={token.image} />
+            <meta name="theme-color" content="#c4005d" />
+            <meta name="twitter:card" content="summary_large_image" />
+          </Head>
+          <CardDetails
+             id={token.id}
+             key={token.id}
+             name={token.name}
+             description={token.description}
+             image={token.image}
+             buyPrice={token.buyPrice}
+             storeId={token.storeId}
+             hash={token.properties.hash}
+             external_url={token.external_url}
+             filename={token.properties.filename}
+             ext={token.properties.ext}
+             totalSupply={token.totalSupply}
+             balance={token.balance}
+             ownerAvatarPreview={token.owner.avatarPreview}
+             ownerUsername={token.owner.username}
+             ownerAddress={token.owner.address}
+             minterAvatarPreview={token.minter.avatarPreview}
+             minterAddress={token.minter.address}
+             minterUsername={token.minter.username}
+             globalState={globalState}
+             networkType='webaverse'
+           />
+        </>
+      :
+        <Loader loading={true} />
+      }
     </>
   )
-}
-
-export async function getServerSideProps(context) {
-  const data = await getToken(context.params.id, context.req.headers.host);
-
-  return { props: { data } }
 }

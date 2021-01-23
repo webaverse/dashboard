73dfe63c7a9de74f70bb5cb8270da18a31185795
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-23 09:08:30 -0600

    metadata on accounts page

--- pages/accounts.js
+++ pages/accounts.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Head from 'next/head';
 import { Container, Row } from 'react-grid-system';
 import { useAppContext } from "../libs/contextLib";
 import { getCreators } from "../functions/UIStateFunctions.js";
@@ -7,22 +8,38 @@ import Loader from "../components/Loader";
 
 export default ({ data }) => {
   const { globalState, setGlobalState } = useAppContext();
-  const [creatorProfiles, setCreatorProfiles] = useState(null);
+  const [creatorProfiles, setCreatorProfiles] = useState(data);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const data = await getCreators();
       setCreatorProfiles(data);
+      setLoading(false);
     })();
   }, []);
 
   return (
     <div className="container">
-      { creatorProfiles ?
-        <ProfileCards profiles={creatorProfiles} />
-      :
+      <Head>
+        <title>Members | Webaverse</title>
+        <meta name="description" content={"Check out the awesome members on Webaverse."} />
+        <meta property="og:title" content={"Members | Webaverse"} />
+        <meta property="og:image" content={"./preview.png"} />
+        <meta name="theme-color" content="#c4005d" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      { loading ?
         <Loader loading={true} />
+      :
+        <ProfileCards profiles={creatorProfiles} />
       }
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const data = await getCreators();
+
+  return { props: { data } }
 }

72f3d80b02bf68b57851c916dcfe7efd6282d62d
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-28 10:35:21 -0600

    add get sidechain activity functions

--- functions/AssetFunctions.js
+++ functions/AssetFunctions.js
@@ -6,6 +6,53 @@ import bip39 from '../libs/bip39.js';
 import hdkeySpec from '../libs/hdkey.js';
 const hdkey = hdkeySpec.default;
 
+export const getTimeByBlock = async (txHash) => {
+  const { web3, getNetworkName } = await getBlockchain();
+  const networkName = getNetworkName() + 'sidechain';
+
+
+  const blockN = await web3[networkName].eth.getTransaction(txHash);
+  console.log("got blockN", blockN);
+  const blockData = await web3[networkName].eth.getBlock(blockN.blockNumber)
+  console.log("got blockData", blockData);
+
+  return blockData.timestamp;
+}
+
+export const getSidechainActivity = async () => {
+  const { web3, contracts, getNetworkName, getMainnetAddress } = await getBlockchain();
+
+  const networkName = getNetworkName();
+  const latest = await web3[networkName + "sidechain"].eth.getBlockNumber();
+  console.log("got latest", latest);
+  const [
+    accountMetadataEntries,
+//    nftProxyDepositedEntries,
+  ] = await Promise.all([
+    contracts['back']['FT'].getPastEvents('Transfer', {
+      fromBlock: latest-200000,
+      toBlock: 'latest',
+    }),
+/*
+    contracts['back']['NFT'].getPastEvents('Transfer', {
+      fromBlock: latest-100000,
+      toBlock: 'latest',
+    }),
+*/
+  ]);
+
+  //let activity = [].concat(accountMetadataEntries, nftProxyDepositedEntries);
+  let activity = [].concat(accountMetadataEntries);
+  activity = await Promise.all(activity.map(async entry => {
+    entry.timestamp = await getTimeByBlock(entry.transactionHash);
+    return entry;
+  }));
+
+  console.log("got accountMetadataEntries", accountMetadataEntries);
+  console.log("got activity", activity);
+  return activity;
+}
+
 export const getStuckAsset = async (tokenName, tokenId, globalState) => {
   if (!globalState.loginToken) return null;
   const { contracts, getNetworkName, getMainnetAddress } = await getBlockchain();

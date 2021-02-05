3802baf84035b28e5c96373ddab943671b5f539a
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-02-04 22:54:24 -0600

    remove comment

--- components/Stripe.js
+++ components/Stripe.js
@@ -3,9 +3,6 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// loadStripe is initialized with your real test publishable API key.
 const promise = loadStripe("pk_test_51HxoDdKz9BHgVnoiMrQ4D5kc9QPx5Vlb9LKMb4uDOod9OkgQY9rbwz3ZIaj9a0GxYzkaZ9PgPtRTGkoHYR7lDa3H00z9Khuvby");
 
 export default () => {

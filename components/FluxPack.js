c4deed17e162e15118187eaaf4d9450d9e136bb8
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-02-06 13:06:48 -0600

    choose blogimg

--- components/FluxPack.js
+++ components/FluxPack.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 
 export default ({ fluxAmount, bonusAmount, costUsd, choosePack, forSale }) => {
+  let blobImg;
+  if (fluxAmount === "50") {
+    blobImg = "/blob.png";
+  } else if (fluxAmount === "100") {
+    blobImg = "/blob2.png";
+  } else if (fluxAmount === "280") {
+    blobImg = "/blob3.png";
+  } else if (fluxAmount === "500") {
+    blobImg = "/blob4.png";
+  } else if (fluxAmount === "1,350") {
+    blobImg = "/blob5.png";
+  }
 
   return (
     <div className="fluxPackContainer">
@@ -14,7 +26,7 @@ export default ({ fluxAmount, bonusAmount, costUsd, choosePack, forSale }) => {
         null
       }
       <div className="fluxPackImageContainer">
-        <img className="fluxPackImage" src="/webaverse.png" />
+        <img src={blobImg} className="fluxPackImage" />
       </div>
       <div className="fluxPackTextContainer">
         <div className={`fluxPackText fluxPackTextAmount`}>{fluxAmount}</div>

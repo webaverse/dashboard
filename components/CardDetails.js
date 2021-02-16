d09179769180e1eff822078e73258a64c7b8a3b3
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-02-16 08:44:16 -0600

    check for correct network type for web3

--- components/CardDetails.js
+++ components/CardDetails.js
@@ -97,7 +97,7 @@ export default ({
       window.web3 = new Web3(window.ethereum);
       window.ethereum.enable();
       const network = await window.web3.eth.net.getNetworkType();
-      if (network === "mainnet") {
+      if (network === "main") {
         return true;
       } else {
         handleError("You need to be on the Mainnet network.");

e2c8e2adc34c238d68f88751a831f4f31b3e0bf0
Avaer Kazmer <a@modules.io>
2021-03-06 19:32:58 -0500

    Update environmentartist.js

--- pages/hiring/environmentartist.js
+++ pages/hiring/environmentartist.js
@@ -15,6 +15,14 @@ export default function Hiring() {
             <li>Intuition for optimizing art assets in a game engine context (materials, textures)</li>
             <li>Understanding of file format basics (e.g. FBX, GLTF)</li>
             <li>Bonus: expereince sketching with the Adobe suite (Illustrator, Photoshop)</li>
+            <li>Samples you might work with:</li>
+            <li>https://sketchfab.com/3d-models/wanderers-f9464c725e1d47e482d988d355da4a7a</li>
+            <li>https://sketchfab.com/3d-models/sci-fi-neon-model-f2637e8e70264eae8694643afd195bfb</li>
+            <li>https://sketchfab.com/3d-models/fantasy-sky-background-15c79bb2fc1147128039fe4ff90fd5a0</li>
+            <li>https://sketchfab.com/3d-models/r1-dun-loireag-b6a6d6f740134384bad0a3e9f835ac68</li>
+            <li>https://sketchfab.com/3d-models/r1-lia-fail-from-the-dothack-imoq-game-series-23e2cfbc3fe145d7b0d5aee500bb75c0</li>
+            <li>https://sketchfab.com/3d-models/net-slum-r1-5aa788c3cb264ac5b367540da4ab8248</li>
+            <li>https://sketchfab.com/3d-models/planetz-icy-02-f448544820084a4f8bd860cdb1292adc</li>
           </ul>
         </div>
         <div>

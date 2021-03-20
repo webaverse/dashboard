32cf8d87c65bc692051b0ac9cc96ab8a98bde93b
Avaer Kazmer <a@modules.io>
2021-03-20 14:01:45 -0400

    Add gameengineer.js

---
+++ pages/hiring/gameengineer.js
@@ -0,0 +1,49 @@
+export default function Hiring() {
+
+  return (
+    <>
+      <img className="hiringLogo" src="/webaverse.png" />
+      <div className="hiringContainer">
+        <div>
+          <h2>WebGL Game Engineer</h2>
+          <p>
+          We are looking for a game developer / web engineer to help us build out user experiences and features.
+          These include wearables, pets, weapons and game elements, support for mobile devices, PVP zones and many, many other features that users will love.
+          </p>
+          <h3>Personality requirements</h3>
+          <p>
+            Self-motivated and self-directed but also communicative
+            Comfortable working in a “greenfield” startup project scenario where you get to take charge
+            Focus on what will make users happy
+            Learns by doing, doesn't wait, asks questions
+            Documents things that are not documented
+            Interested in science fiction technology, i.e. VR, AI, blockchain, etc
+            Capable of delivering and meeting deadlines without supervision
+            Willing to embed with team and build long-term products and relationships
+          </p>
+          <h3>Technology</h3>
+          <ul>
+            <li>THREE.js</li>
+            <li>WebGL</li>
+            <li>JavaScript</li>
+            <li>WASM</li>
+            <li>WebRTC</li>
+            <li>React / Mithril</li>
+            <li>WebRTC</li>
+            <li>Strong browser knowledge</li>
+            <li>Understanding of graphics pipeline</li>
+            <li>GLSL/shaders</li>
+            <li>An eye for fun</li>
+            <li>An eye for design</li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <h2>Next steps</h2>
+          <p>Send us an email at <a href="mailto:hello@webaverse.com">hello@webaverse.com</a></p>
+          <p>or join our Discord <a target="_blank" href="https://discord.gg/R5wqYhvv53">here</a>.</p>
+        </div>
+      </div>
+    </>
+  )
+}

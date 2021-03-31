6b200bf56227f0c994c47f16ae8281cda4c019a9
Avaer Kazmer <a@modules.io>
2021-03-31 01:38:37 -0400

    Update blockchain.js

--- webaverse/blockchain.js
+++ webaverse/blockchain.js
@@ -90,7 +90,7 @@ export const isTokenOnMain = async id => {
   return tokenOnMain;
 };
 
-let injectedWeb3 = (typeof window !== 'undefined' && window.ethereum) ?
+const injectedWeb3 = (typeof window !== 'undefined' && window.ethereum) ?
   new Web3(window.ethereum)
 :
   new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${infuraKey}`))

e2162560b64442fe6408ea40a584530904effb1e
Avaer Kazmer <a@modules.io>
2021-04-05 17:28:59 -0400

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -15,32 +15,32 @@ export default () => {
   const [error, setError] = useState(null);
 
   const loginWithKey = (key, play, realmId) => {
-    console.log('loginWithKey 1');
+    // console.log('loginWithKey 1');
     if (bip39.validateMnemonic(key)) {
-      console.log('loginWithKey 2');
+      // console.log('loginWithKey 2');
       loginWithPrivateKey(key, globalState)
         .then(res => {
-          console.log('loginWithKey 3');
+          // console.log('loginWithKey 3');
           setInitialState(res, key, play, realmId);
         })
         .catch(err => {
           console.warn('loginWithKey error', err);
         });
     } else {
-      alert("not a valid private key!");
+      alert('not a valid private key!');
     }
   }
 
   const setInitialState = async (state, key, play, realmId) => {
-    console.log('setInitialState 1');
+    // console.log('setInitialState 1');
     
     const balance = await getBalance(state.address);
-    console.log('setInitialState 2');
+    // console.log('setInitialState 2');
     const newState = await pullUser(state);
-    console.log('setInitialState 3');
+    // console.log('setInitialState 3');
 
     if (play) {
-      console.log('setInitialState 4.1');
+      // console.log('setInitialState 4.1');
       const storedLoginToken = await storage.get("loginToken");
 
       if (storedLoginToken.mnemonic && storedLoginToken.mnemonic != state.loginToken.mnemonic) {
@@ -57,7 +57,7 @@ export default () => {
         window.location.href = "https://app.webaverse.com";
       }
     } else {
-      console.log('setInitialState 4.2');
+      // console.log('setInitialState 4.2');
       const storedLoginToken = await storage.get("loginToken");
 
       if (storedLoginToken.mnemonic && storedLoginToken.mnemonic != state.loginToken.mnemonic) {
@@ -67,12 +67,12 @@ export default () => {
         }
       }
       
-      console.log('setInitialState 5');
+      // console.log('setInitialState 5');
 
       setGlobalState({ balance, loginProcessed: true, login: "true", ...newState });
-      console.log('setInitialState 6');
+      // console.log('setInitialState 6');
       router.push("/accounts/" + state.address);
-      console.log('setInitialState 7');
+      // console.log('setInitialState 7');
     }
   }
 
@@ -83,25 +83,25 @@ export default () => {
     const realmId = new URLSearchParams(window.location.search).get("realmId") || "";
     const arrivingFromTwitter = new URLSearchParams(window.location.search).get("twitter") || false;
 
-    console.log('effect 1');
+    // console.log('effect 1');
 
     if (code || id || play) {
-      console.log('effect 2');
+      // console.log('effect 2');
       try {
         const res = await fetch(
           arrivingFromTwitter ?
           `https://login.exokit.org/?twittercode=${code}&twitterid=${id}` :
           `https://login.exokit.org/?discordcode=${code}&discordid=${id}`, {method: 'POST'});
-        console.log('effect 3');
+        co// nsole.log('effect 3');
         if (res.status !== 200) {
           throw "Login did not work, got response: " + res.status;
         }
-        console.log('effect 4');
+        // console.log('effect 4');
         const j = await res.json();
-        console.log('effect 5');
+        // console.log('effect 5');
         const {mnemonic} = j;
         if (mnemonic) {
-          console.log('effect 6');
+          // console.log('effect 6');
           loginWithKey(mnemonic, play, realmId);
         } else {
           console.warn('no mnemonic returned from api');

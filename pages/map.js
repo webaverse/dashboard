83d22738145723ea2322ef11c2a643c5bce9e2ec
Avaer Kazmer <a@modules.io>
2021-04-19 10:16:29 -0400

    More checkpointing

--- pages/map.js
+++ pages/map.js
@@ -6,9 +6,7 @@ const Map = () => {
       <p>
         Double click anywhere on the map to go directly there in Webaverse.
       </p>
-      <div className="IFrameContainer">
-        <iframe className="IFrame" src="https://map.webaverse.com" />
-      </div>
+      <iframe className="map-iframe" src="https://map.webaverse.com" />
     </>
   )
 };

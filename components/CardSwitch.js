a95562d257b0efa5c55babe17bc0b9f5dca5e38b
Avaer Kazmer <a@modules.io>
2021-04-18 23:30:26 -0400

    Create CardSwitch.js

---
+++ components/CardSwitch.js
@@ -0,0 +1,32 @@
+import AssetCardSvg from './CardSvg';
+import AssetCard2D from './Card2D';
+import AssetCard3D from './Card3D';
+import AssetCardLive from './CardLive';
+
+const CardSwitch = (props) => {
+  const {selectedView} = props;
+  switch (selectedView) {
+    case 'cards': {
+      return (
+        <AssetCardSvg {...props} />
+      );
+    }
+    case '2d': {
+      return (
+        <AssetCard2D {...props} />
+      );
+    }
+    case '3d': {
+      return (
+        <AssetCard3D {...props} />
+      );
+    }
+    case 'live': {
+      return (
+        <AssetCardLive {...props} />
+      );
+    }
+  }
+};
+
+export default CardSwitch;

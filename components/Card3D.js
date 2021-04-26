bad4b28848bab681be904570779641a86412af35
Avaer Kazmer <a@modules.io>
2021-04-26 00:43:07 -0400

    Update Card3D.js

--- components/Card3D.js
+++ components/Card3D.js
@@ -2,88 +2,88 @@ import React, {useState} from "react";
 // import procgen, {types} from '../webaverse/procgen.js';
 
 const Card3D = ({
-    id,
-    assetName,
-    description,
-    image,
-    hash,
-    animation_url,
-    ext,
-    totalSupply,
-    minterAvatarPreview,
-    minterUsername,
-    cardSize,
-    isMainnet,
-    isPolygon,
-    glow,
-    imageView,
-    // cardSvgSource,
+  id,
+  assetName,
+  description,
+  image,
+  hash,
+  animation_url,
+  ext,
+  totalSupply,
+  minterAvatarPreview,
+  minterUsername,
+  cardSize,
+  isMainnet,
+  isPolygon,
+  glow,
+  imageView,
+  // cardSvgSource,
 }) => {
-    const [perspective, setPerspective] = useState([false, false]);
-    const [flip, setFlip] = useState(false);
-    const [transitioning, setTransitioning] = useState(false);
-    const [boundingBox, setBoundingBox] = useState(null);
-  
-    /* let video = false;
-    if (["webm", "mp4"].indexOf(ext) >= 0) {
-        image = animation_url;
-        video = true;
-    } else if (ext === "gif") {
-        image = animation_url;
-    }
+  const [perspective, setPerspective] = useState([false, false]);
+  const [flip, setFlip] = useState(false);
+  const [transitioning, setTransitioning] = useState(false);
+  const [boundingBox, setBoundingBox] = useState(null);
 
-    let networkIcon;
-    if (isMainnet) {
-        networkIcon = "/icon-ethereum.svg";
-    } else if(isPolygon) {
-        networkIcon = "/icon-polygon.svg";
-    } else {
-        networkIcon = "/icon-webaverse.svg";
-    }
+  /* let video = false;
+  if (["webm", "mp4"].indexOf(ext) >= 0) {
+      image = animation_url;
+      video = true;
+  } else if (ext === "gif") {
+      image = animation_url;
+  }
 
-    let extIcon;
-    if (ext.toLowerCase() === "png") {
-        extIcon = "/icon-jpg.svg";
-    } else if (ext.toLowerCase() === "jpg" || ext.toLowerCase() === "jpeg") {
-        extIcon = "/icon-jpg.svg";
-    } else if (ext.toLowerCase() === "vrm") {
-        extIcon = "/icon-vrm.svg";
-    } else if (ext.toLowerCase() === "vox") {
-        extIcon = "/icon-vrm.svg";
-    } else if (ext.toLowerCase() === "wbn") {
-        extIcon = "/icon-vrm.svg";
-    } else if (ext.toLowerCase() === "glb") {
-        extIcon = "/icon-glb.svg";
-    } else if (ext.toLowerCase() === "gif") {
-        extIcon = "/icon-gif.svg";
-    }
+  let networkIcon;
+  if (isMainnet) {
+      networkIcon = "/icon-ethereum.svg";
+  } else if(isPolygon) {
+      networkIcon = "/icon-polygon.svg";
+  } else {
+      networkIcon = "/icon-webaverse.svg";
+  }
 
-    let rarity;
-    if (totalSupply < 2) {
-        rarity = "unique";
-    } else if (totalSupply < 3) {
-        rarity = "mythic";
-    } else if (totalSupply < 8) {
-        rarity = "legendary";
-    } else if (totalSupply < 15) {
-        rarity = "epic";
-    } else if (totalSupply < 30) {
-        rarity = "rare";
-    } else if (totalSupply < 50) {
-        rarity = "uncommon";
-    } else if (totalSupply > 50) {
-        rarity = "common";
-    } */
+  let extIcon;
+  if (ext.toLowerCase() === "png") {
+      extIcon = "/icon-jpg.svg";
+  } else if (ext.toLowerCase() === "jpg" || ext.toLowerCase() === "jpeg") {
+      extIcon = "/icon-jpg.svg";
+  } else if (ext.toLowerCase() === "vrm") {
+      extIcon = "/icon-vrm.svg";
+  } else if (ext.toLowerCase() === "vox") {
+      extIcon = "/icon-vrm.svg";
+  } else if (ext.toLowerCase() === "wbn") {
+      extIcon = "/icon-vrm.svg";
+  } else if (ext.toLowerCase() === "glb") {
+      extIcon = "/icon-glb.svg";
+  } else if (ext.toLowerCase() === "gif") {
+      extIcon = "/icon-gif.svg";
+  }
 
-    return (
-      <iframe
-        className="content-preview-3d"
-        src={"https://app.webaverse.com/?t=" + id}
-        onWheel={e => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
-      />
-    );
+  let rarity;
+  if (totalSupply < 2) {
+      rarity = "unique";
+  } else if (totalSupply < 3) {
+      rarity = "mythic";
+  } else if (totalSupply < 8) {
+      rarity = "legendary";
+  } else if (totalSupply < 15) {
+      rarity = "epic";
+  } else if (totalSupply < 30) {
+      rarity = "rare";
+  } else if (totalSupply < 50) {
+      rarity = "uncommon";
+  } else if (totalSupply > 50) {
+      rarity = "common";
+  } */
+
+  return (
+    <iframe
+      className="content-preview-3d"
+      src={"https://app.webaverse.com/?t=" + id}
+      onWheel={e => {
+        e.preventDefault();
+        e.stopPropagation();
+      }}
+    />
+  );
 };
 export default Card3D;

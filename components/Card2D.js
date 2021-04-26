0a2f44a7842d7025c0d9cd63d5e8facbe88cca0d
Avaer Kazmer <a@modules.io>
2021-04-26 01:40:07 -0400

    Update Card2D.js

--- components/Card2D.js
+++ components/Card2D.js
@@ -25,56 +25,6 @@ const Card2D = ({
     const [boundingBox, setBoundingBox] = useState(null);
     const [dimensions, setDimensions] = useState(null);
   
-    /* let video = false;
-    if (["webm", "mp4"].indexOf(ext) >= 0) {
-        image = animation_url;
-        video = true;
-    } else if (ext === "gif") {
-        image = animation_url;
-    }
-
-    let networkIcon;
-    if (isMainnet) {
-        networkIcon = "/icon-ethereum.svg";
-    } else if(isPolygon) {
-        networkIcon = "/icon-polygon.svg";
-    } else {
-        networkIcon = "/icon-webaverse.svg";
-    }
-
-    let extIcon;
-    if (ext.toLowerCase() === "png") {
-        extIcon = "/icon-jpg.svg";
-    } else if (ext.toLowerCase() === "jpg" || ext.toLowerCase() === "jpeg") {
-        extIcon = "/icon-jpg.svg";
-    } else if (ext.toLowerCase() === "vrm") {
-        extIcon = "/icon-vrm.svg";
-    } else if (ext.toLowerCase() === "vox") {
-        extIcon = "/icon-vrm.svg";
-    } else if (ext.toLowerCase() === "wbn") {
-        extIcon = "/icon-vrm.svg";
-    } else if (ext.toLowerCase() === "glb") {
-        extIcon = "/icon-glb.svg";
-    } else if (ext.toLowerCase() === "gif") {
-        extIcon = "/icon-gif.svg";
-    }
-
-    let rarity;
-    if (totalSupply < 2) {
-        rarity = "unique";
-    } else if (totalSupply < 3) {
-        rarity = "mythic";
-    } else if (totalSupply < 8) {
-        rarity = "legendary";
-    } else if (totalSupply < 15) {
-        rarity = "epic";
-    } else if (totalSupply < 30) {
-        rarity = "rare";
-    } else if (totalSupply < 50) {
-        rarity = "uncommon";
-    } else if (totalSupply > 50) {
-        rarity = "common";
-    } */
     const _loadDimensions = el => {
       const {naturalWidth, naturalHeight} = el;
       if (!dimensions || dimensions[0] !== naturalWidth || dimensions[1] !== naturalHeight) {

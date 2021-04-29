d02cf8c7e2fc33250e0c267c1502216a88fa5ef9
Avaer Kazmer <a@modules.io>
2021-04-29 16:28:30 -0400

    Create ViewSwitch.js

---
+++ components/ViewSwitch.js
@@ -0,0 +1,31 @@
+const ViewSwitch = ({
+  selectedView,
+  setSelectedView,
+}) => {
+  return (
+    <div className="navbarSwitchWrap">
+      <div className="navbarSwitch">
+        <div className={`option ${selectedView === 'cards' ? 'selected' : ''}`} onClick={e => {
+          setSelectedView('cards');
+        }}>
+          Cards
+        </div>
+        <div className={`option ${selectedView === '2d' ? 'selected' : ''}`} onClick={e => {
+          setSelectedView('2d');
+        }}>
+          2D
+        </div>
+        <div className={`option ${selectedView === '3d' ? 'selected' : ''}`} onClick={e => {
+          setSelectedView('3d');
+        }}>
+          3D
+        </div>
+        <div className={`option ${selectedView === 'live' ? 'selected' : ''}`} onClick={e => {
+          setSelectedView('live');
+        }}>
+          Live
+        </div>
+      </div>
+    </div>
+  );
+};

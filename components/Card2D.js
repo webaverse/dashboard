d5c85eb50404579b5de336c10c447ca7b9d3d958
Avaer Kazmer <a@modules.io>
2021-04-29 17:21:20 -0400

    Update Card2D.js

--- components/Card2D.js
+++ components/Card2D.js
@@ -2,70 +2,70 @@ import React, {useState} from "react";
 // import procgen, {types} from '../webaverse/procgen.js';
 
 const Card2D = ({
-    id,
-    assetName,
-    description,
-    image,
-    hash,
-    animation_url,
-    ext,
-    totalSupply,
-    minterAvatarPreview,
-    minterUsername,
-    cardSize,
-    isMainnet,
-    isPolygon,
-    glow,
-    imageView,
-    // cardSvgSource,
+  id,
+  assetName,
+  description,
+  image,
+  hash,
+  animation_url,
+  ext,
+  totalSupply,
+  minterAvatarPreview,
+  minterUsername,
+  cardSize,
+  isMainnet,
+  isPolygon,
+  glow,
+  imageView,
+  // cardSvgSource,
 }) => {
-    const [perspective, setPerspective] = useState([false, false]);
-    const [flip, setFlip] = useState(false);
-    const [transitioning, setTransitioning] = useState(false);
-    const [boundingBox, setBoundingBox] = useState(null);
-    const [dimensions, setDimensions] = useState(null);
+  const [perspective, setPerspective] = useState([false, false]);
+  const [flip, setFlip] = useState(false);
+  const [transitioning, setTransitioning] = useState(false);
+  const [boundingBox, setBoundingBox] = useState(null);
+  const [dimensions, setDimensions] = useState(null);
+
+  const _loadDimensions = el => {
+    const {naturalWidth, naturalHeight} = el;
+    if (!dimensions || dimensions[0] !== naturalWidth || dimensions[1] !== naturalHeight) {
+      setDimensions([naturalWidth, naturalHeight]);
+    }
+  };
+  const _cancelDragStart = e => {
+    e.preventDefault();
+  };
   
-    const _loadDimensions = el => {
-      const {naturalWidth, naturalHeight} = el;
-      if (!dimensions || dimensions[0] !== naturalWidth || dimensions[1] !== naturalHeight) {
-        setDimensions([naturalWidth, naturalHeight]);
-      }
-    };
-    const _cancelDragStart = e => {
-      e.preventDefault();
-    };
-    
-    // render gifs as gifs
-    image = image.replace(/\.gif\/preview\.png$/, '.gif/preview.gif');
-    
-    return (
-      <img
-        src={image}
-        className="content-preview-2d"
-        onDragStart={_cancelDragStart}
-        onDoubleClick={e => {
-          e.target.requestFullscreen();
-        }}
-        ref={async el => {
-          if (el) {
-            if (!el.complete) {
-              await new Promise((accept, reject) => {
-                el.addEventListener('load', e => {
-                  accept();
-                });
-                el.addEventListener('error', err => {
-                  reject(err);
-                });
+  // render gifs as gifs
+  image = image.replace(/\.gif\/preview\.png$/, '.gif/preview.gif');
+  
+  return (
+    <img
+      src={image}
+      className="content-preview-2d"
+      onDragStart={_cancelDragStart}
+      onDoubleClick={e => {
+        e.target.requestFullscreen();
+      }}
+      ref={async el => {
+        if (el) {
+          if (!el.complete) {
+            await new Promise((accept, reject) => {
+              el.addEventListener('load', e => {
+                accept();
               });
-            }
-            _loadDimensions(el);
+              el.addEventListener('error', err => {
+                reject(err);
+              });
+            });
           }
-        }}
-        style={dimensions ? {
-          maxWidth: dimensions[0],
-          maxHeight: dimensions[1],
-        } : null}
-      />
-    );
+          _loadDimensions(el);
+        }
+      }}
+      style={dimensions ? {
+        maxWidth: dimensions[0],
+        maxHeight: dimensions[1],
+      } : null}
+    />
+  );
 };
 export default Card2D;

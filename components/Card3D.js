59f3fb71de094c91a68156d247cd0be990f29cbf
Avaer Kazmer <a@modules.io>
2021-04-30 21:38:29 -0400

    Update Card3D.js

--- components/Card3D.js
+++ components/Card3D.js
@@ -1,13 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
+// import { createPortal } from 'react-dom'
 import {appPreviewHost} from '../webaverse/constants';
 import AssetCardSvg from './CardSvg.js';
+import ProgressBar from './ProgressBar.js';
+
+/* export const IFrame = ({
+  children,
+  ...props
+}) => {
+  const [contentRef, setContentRef] = useState(null)
+  const mountNode =
+    contentRef?.contentWindow?.document?.body
+
+  return (
+    <iframe {...props} ref={el => {
+      setContentRef(el);
+      el.addEventListener('load', e => {
+        console.log('got load', e);
+        props.onLoad(e);
+      });
+      el.addEventListener('load', err => {
+        props.onError(err);
+        console.log('got load', err);
+      });
+    }}>
+      {mountNode && createPortal(children, mountNode)}
+    </iframe>
+  )
+}; */
 
 const Card3D = props => {
   const {
     id,
     hash,
     ext,
-    loaded,
+    // loaded,
     cardSize,
     open,
     onClick,
@@ -15,6 +42,7 @@ const Card3D = props => {
   
   // const [open, setOpen] = useState(false);
   const [locked, setLocked] = useState(true);
+  const [loaded, setLoaded] = useState(false);
   
   // `{storageHost}/ipfs/${hash}`
   const qs = {
@@ -36,9 +64,28 @@ const Card3D = props => {
     }
   }
   
+  useEffect(() => {
+    if (!open) {
+      if (!locked) {
+        setLocked(true);
+      }
+      if (loaded) {
+        setLoaded(false);
+      }
+    }
+  }, [open, locked, loaded]);
+  
   const makeIframe = () => (<iframe
     className={`iframe ${loaded ? 'loaded' : ''} ${(open && locked) ? 'locked' : ''}`}
     src={src}
+    onLoad={e => {
+      console.log('iframe onload', e);
+      setLoaded(true);
+    }}
+    onError={e => {
+      console.warn(e);
+      setLoaded(true);
+    }}
   />);
   
   return (cardSize === 'small' ?
@@ -58,10 +105,17 @@ const Card3D = props => {
               setLocked(false);
             }
           }}
-        />
-      : 
-        makeIframe()
-      }
+        >Scroll to load</div>
+      : (
+        <Fragment>
+          {!loaded ?
+            <div className="progress-bar-wrap">
+              <ProgressBar />
+            </div>
+          : null}
+          {makeIframe()}
+        </Fragment>
+      )}
       <div className="card-sub-wrap">
         <div className="top">
           <AssetCardSvg

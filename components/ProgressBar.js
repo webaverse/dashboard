be65546fa9ee0ea0ff2edda09bebadce05a7de03
Avaer Kazmer <a@modules.io>
2021-04-30 21:09:59 -0400

    Update ProgressBar.js

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import {useState} from 'react';
+import {schedulePerFrame} from "../webaverse/util";
 
 const ProgressBar = ({
   className,
   style,
-  value,
+  value: initialValue,
 }) => {
-    return (
-        <div
-          className={`progress-bar ${className || ''}`}
-          style={style}
-        >
-          <div className="progress-bar-inner" style={{
-            transform: `scaleX(${value})`,
-          }} />
-        </div>
-    );
+  const [value, setValue] = useState(initialValue);
+  
+  const _updateMintProgress = () => {
+    if (typeof initialValue === 'number') {
+      let frame = null;
+      let startTime = 0;
+      const _scheduleFrame = () => {
+        frame = requestAnimationFrame(_recurse);
+      };
+      const _recurse = () => {
+        _scheduleFrame();
+        // if (mintMenuStep === 3) {
+          /* const _makeValue = () => {
+            const now = Date.now();
+            const f = 1 + Math.sin((now - startTime)/1000 * Math.PI) / 2;
+            return (-1 + Math.random() * 2) * 10 * f;
+          };
+          setJitter([
+            _makeValue(),
+            _makeValue(),
+          ]); */
+          setValue(Date.now() % 1000 / 1000);
+        // }
+      };
+      schedulePerFrame(() => {
+        startTime = Date.now();
+        _scheduleFrame();
+      }, () => {
+        frame && cancelAnimationFrame(frame);
+        frame = null;
+      });
+    }
+  };
+  _updateMintProgress();
+  
+  return (
+    <div
+      className={`progress-bar ${className || ''}`}
+      style={style}
+    >
+      <div className="progress-bar-inner" style={{
+        transform: `scaleX(${value})`,
+      }} />
+    </div>
+  );
 };
 export default ProgressBar;

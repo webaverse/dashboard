9f1be4d27f92a5d38e66601bd51aec9d54e1f79e
Avaer Kazmer <a@modules.io>
2021-05-01 20:32:44 -0400

    Update accounts.js

--- pages/accounts.js
+++ pages/accounts.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Head from 'next/head';
-import {Container, Row} from 'react-grid-system';
+// import {Container, Row} from 'react-grid-system';
 import {useAppContext} from "../libs/contextLib";
 import {getCreators} from "../functions/UIStateFunctions.js";
 import ProfileCards from "../components/ProfileCards";

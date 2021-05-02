ebf8ef98802e40d2994784ffd5ec8a0918df8c5f
Avaer Kazmer <a@modules.io>
2021-05-01 20:56:06 -0400

    Update Clip.js

--- components/Clip.js
+++ components/Clip.js
@@ -1,8 +1,26 @@
+import {useState} from 'react';
+
 const Clip = props => {
-  return (
-    <video {...props} key={props.src}>
+  const [loaded, setLoaded] = useState(false);
+  const [errored, setErrored] = useState(false);
+  
+  return (!errored ?
+    <video
+      {...props}
+      onLoad={e => {
+        setLoaded(true);
+      }}
+      onError={e => {
+        setLoaded(true);
+        setErrored(true);
+      }}
+    >
       <source src={props.src} />
     </video>
+  :
+    <div className="clip-error">
+      <img className="icon" src="/error.svg" />
+    </div>
   );
 };
 export default Clip;

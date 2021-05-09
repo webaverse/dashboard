e722816ff184695955da16a9483a170721a6034b
Avaer Kazmer <a@modules.io>
2021-05-09 04:21:45 -0400

    Update CardDetails.js

--- components/CardDetails.js
+++ components/CardDetails.js
@@ -474,6 +474,9 @@ const TransferMenu = ({
     </div>
   );
 };
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+  return [...new Uint8Array(buffer)].map(x => x.toString(16).padStart(2, '0')).join('');
+}
 const BurnMenu = ({
   id,
   globalState,
